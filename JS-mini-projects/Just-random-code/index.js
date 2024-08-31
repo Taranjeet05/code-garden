85f206dd2cc63f552a30ad973b5f687d4fa00a8c
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-09-01 01:08:44 +0200

    Add tasks for practicing JavaScript concepts: arrays, objects, arrow functions, map, filter, reduce, and destructuring

--- JS-mini-projects/Just-random-code/index.js
+++ JS-mini-projects/Just-random-code/index.js
@@ -1,7 +1,4 @@
 
-
-
-
 function calculateTax (cost, taxPercent = 0.1) {
   console.log(cost * taxPercent); 
 }
@@ -313,3 +310,173 @@ let concatenateAllStrings = new_Words.reduce((accumulator, value) => {
 }, ""); 
 
 console.log(concatenateAllStrings); 
+
+console.log ('..............................................................................')
+console.log ('..............................................................................')
+console.log ('..............................................................................')
+console.log ('..............................................................................')
+console.log ('..............................................................................')
+
+
+/* Before just going to code in react I decide to go through simple concept of js which
+gonna help me in React. 
+The Topics are :-
+1.Array :: (push(), pop(), length)
+2.Object :: (Accessing Values, Adding/Updating Values)
+3.Arrow functions :: (Basic syntax)
+4.Map / filter / reduce
+5.Mutability and immutability
+6.Important :: Destructuring  */
+
+// Now I will Do 2 -2 tasks for easch topic. 
+
+/*1. Arrays
+
+Task 1: Favorite Colors
+
+Create an array called favoriteColors with at least 5 colors 
+that you like. Then, use array indexing to log the first and last color to the console.*/
+
+let favoriteColors = ['yellow', 'Green', 'Orange', 'white', 'black']; 
+
+console.log(favoriteColors[0]);
+console.log(favoriteColors[favoriteColors.length-1]);
+
+/*
+Task 2: Add a New Hobby
+
+You have an array called hobbies with some of your hobbies in it. Add a new hobby to 
+the end of the array using the push method and log the updated array.*/
+
+let hobbies = ['Politics', 'history', 'geography', 'coding', 'Exploring India'];
+
+hobbies.push('Meditating');
+
+console.log(hobbies);
+
+/*2. Objects
+Task 1: Create Your Profile
+
+Create an object called myProfile with properties like
+ name, age, favoriteFood, and hobby. Then, log your name and favorite food to the console.*/
+
+ let my_Profile = {
+  name: 'Narendera Modi',
+  age: 74,
+  favoritefood: 'Indian',
+  hobby: 'Desh Ki seva',
+  role: 'Prime Minister Of India'
+ };
+
+ console.log(`${my_Profile.name} and favorite food ${my_Profile.favoritefood}`);
+
+
+/*3. Arrow Functions
+
+Task 1: Simple Addition
+
+Write an arrow function called addNumbers that takes two 
+numbers as parameters and returns their sum. Test it by adding 5 and 3 and log the result. */
+
+
+const adding = (a, b) => a + b;
+
+console.log(adding(5, 3))
+
+/*Task 2: Greet a Friend
+
+Write an arrow function called greetFriend that takes a friend’s name as a parameter 
+and returns a greeting like "Hello, [name]!". Use it to greet a friend and log the greeting. */
+
+const greetFriend = name => `Hello, ${name}!`;
+
+console.log(greetFriend('Rahul Sharma'));
+
+/* 4. Map and Filter
+
+Task 1: Double the Numbers
+
+Create an array of numbers from 1 to 5. Use the map function 
+to create a new array where each number is doubled. Log the new array to the console.*/
+
+let arrNumber = [1, 2, 3, 4, 5];
+
+let doubleTheNumberInArray = arrNumber.map((value) => {
+  return value * 2;
+});
+
+console.log(doubleTheNumberInArray);
+
+/*Task 2: Filter Short Names
+
+Create an array of names. Use the filter function to create a new 
+array with only names that are longer than 4 characters. Log the new array to the console. */
+
+let arrayName = ['Rahul', 'Taranjeet', 'Ram', 'Krishna', 'Tom'];
+
+let filter = arrayName.filter((value) => {
+    return value.length >= 4
+});
+
+console.log(filter);
+
+/*  Mutability and Immutability
+
+Task 1: Mutate an Array
+
+Create an array of fruits. Use push to add a new fruit 
+to the array. Then, log the original array to the console and see how it has changed. */
+
+let newarray = ['orange', 'apple', 'Mango'];
+
+newarray.push('apple2');
+
+console.log(newarray);
+
+
+/*Task 2: Create an Immutable Array
+
+Create an array of numbers. Use the spread operator (...) to create a new array that 
+adds one more number to the original array without changing the original array. 
+Log both arrays to see the difference. */
+
+console.log('Immutable');
+
+let numm = [1, 2, 3, 4, 5];
+
+let numm2= [...numm, 56, 46];
+
+console.log(numm);
+console.log(numm2);
+
+
+/*6. Destructuring
+
+Task 1: Destructure an Array
+
+Create an array called fruitBasket with three fruits in it. 
+Use array destructuring to assign each fruit to its own variable, and log each variable.*/
+
+console.log('...........................');
+console.log('destructuring');
+
+let fruitBasket = ['apple', 'apple2', 'apple3'];
+
+let [f1, f2, f3] = fruitBasket;
+
+console.log(f1, f2, f3);
+
+/*Task 2: Destructure an Object
+
+Create an object called car with properties like make, model, and year. 
+Use object destructuring to create variables for make and model, and log them.
+ */
+
+let carWithProperties = {
+  model: 'xyz',
+  year: 2005,
+};
+
+let {model, year} = carWithProperties;
+console.log(model);
+console.log(year);
