40a53e9881a19b5c78f7663d71caee0f1b123ee4
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-09-20 10:15:44 +0200

    5 kyu peter, the baker.. codewars

--- JS-mini-projects/Just-random-code/index.js
+++ JS-mini-projects/Just-random-code/index.js
@@ -417,3 +417,20 @@ function top3(products, amounts, prices) {
     .slice(0, 3)
     .map(item => item.product);
 }
+
+console.log('......::::::.......');
+
+function cakes(recipe, available) {
+  let maxCakes = Infinity;
+
+  for (let ingredient in recipe) {
+   
+    if (!available[ingredient]) {
+      return 0;
+    } 
+    const possibleCakes = Math.floor(available[ingredient] / recipe[ingredient]);
+    maxCakes = Math.min(maxCakes, possibleCakes);
+  }
+  
+  return maxCakes;
+}
