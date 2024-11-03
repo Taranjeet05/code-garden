1ca4abfb53fa63cdcff03ea5bb2c555c99ba0351
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-11-03 20:15:58 +0100

    feat: create Meme component with input fields, button, and added flexbox styling

---
+++ React-mini-projects/meme-generator/src/components/Meme.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Meme = () => {
+  return (
+    <main>
+      <form>
+        <input type='text'
+        placeholder='Top-text'
+        />
+    
+        <input type='text'
+        placeholder='Bottom-text'
+        />
+
+        <button>Get a new meme Image🖼️</button>
+    </form>
+    </main>
+
+  )
+}
+
+export default Meme
