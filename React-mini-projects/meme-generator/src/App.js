402acc823a722f62dac9d38d09b1f3d02ee79fa2
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-11-08 18:26:15 +0100

    Clean up unused components for revision

--- React-mini-projects/meme-generator/src/App.js
+++ React-mini-projects/meme-generator/src/App.js
@@ -1,18 +1,12 @@
 import './App.css';
 import Header from './components/Header';
 import Meme from './components/Meme';
-import Example from './components/Example';
-import ObjectState from './components/ObjectState';
-import Box from './components/Box';
 
 function App() {
   return (
   <div>
     <Header />
     <Meme />
-    <Example />
-    <ObjectState />
-    <Box />
   </div>
   );
 }
