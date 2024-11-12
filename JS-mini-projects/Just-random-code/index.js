e6df06e06c5cb8fcd8d917d8a9175bc1e31428d3
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-11-12 23:25:38 +0100

    Complete Codewars task #4

--- JS-mini-projects/Just-random-code/index.js
+++ JS-mini-projects/Just-random-code/index.js
@@ -89,3 +89,21 @@ function guessGifts(wishlist, presents) {
 
   return guessedGifts;
 }
+
+//Task 4 :: codewar 
+
+function cakes(recipe, available) {
+  let maxCakes = Infinity;
+
+  for (let ingredient in recipe) {
+   
+    if (!available[ingredient]) {
+      return 0;
+    } 
+    const possibleCakes = Math.floor(available[ingredient] / recipe[ingredient]);
+    maxCakes = Math.min(maxCakes, possibleCakes);
+  }
+  
+  return maxCakes;
+}
+
