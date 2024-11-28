7e15e9adc224e43a4c74e1a2f71e8a7849aa4d1f
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-11-28 16:28:45 +0100

    Added light/dark mode toggle with button in header and styled themes

--- React-mini-projects/react-info-site/src/App.js
+++ React-mini-projects/react-info-site/src/App.js
@@ -1,24 +1,29 @@
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
-
+import { useState } from 'react'; 
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false); 
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode); 
+  };
+
   return (
-    <>
-      <Header/>
+    <div className={isDarkMode ? 'dark-mode' : 'light-mode'}> 
+      <Header toggleTheme={toggleTheme} /> 
       <h1 className='heading'>Fun facts about React</h1>
       <ul className='points'>
-        <li>was first realesed in 2013</li>
-        <li>was originally created by jordan walke</li>
-        <li>Has well over 100k stars on Github</li>
+        <li>was first released in 2013</li>
+        <li>was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on GitHub</li>
         <li>is maintained by Facebook</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
       </ul>
-      <Footer/>
-    </>
+      <Footer />
+    </div>
   );
 }
 
 export default App;
-
--- React-mini-projects/react-info-site/src/components/Footer.js
+++ React-mini-projects/react-info-site/src/components/Footer.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
     <>
-    <small className='footer-text'>2024 Taranjeet Singh. All right reserved. </small>
+      <small className='footer-text'>2024 Taranjeet Singh. All rights reserved. </small>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
