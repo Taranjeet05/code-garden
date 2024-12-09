8e40c9c5420149b083b3d02d963a2c000da38169
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-12-09 15:24:53 +0100

    feat: add Random-Color component

--- React-mini-projects/ReactDojo/src/App.jsx
+++ React-mini-projects/ReactDojo/src/App.jsx
@@ -1,14 +1,16 @@
-import './App.css'
-import Accordion from './components/accordion'
+import "./App.css";
+import Accordion from "./components/accordion";
+import RandomColor from "./components/random-color";
 
 function App() {
-
   return (
     <>
-    {/* Accordion */}
-    <Accordion />
+      {/* Accordion */}
+      <Accordion />
+      {/*Random-Color */}
+      <RandomColor />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ React-mini-projects/ReactDojo/src/components/random-color/index.jsx
@@ -0,0 +1,13 @@
+
+
+const RandomColor = () => {
+  return (
+    <>
+      <div className="container">
+        <h1>Random-Color</h1>
+      </div>
+    </>
+  );
+};
+
+export default RandomColor;
