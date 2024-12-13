b3e618f98a4dc1bfcf063b500eef71bf212f3382
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-12-13 19:51:18 +0100

    feat: add useEffect for disabling button and loading state message

--- React-mini-projects/ReactDojo/src/components/load-more-data/index.jsx
+++ React-mini-projects/ReactDojo/src/components/load-more-data/index.jsx
@@ -1,53 +1,56 @@
-import { useState, useEffect } from "react"
-
+import { useState, useEffect } from "react";
 
 const LoadMoreData = () => {
-
   // usestates
-const [loading, setLoading] = useState(false);
-const [products, setProducts] = useState([]);
-const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
+  const [disableButton, setDisableButton] = useState(false);
 
-async function fetchProducts() {
-  try {
-    setLoading(true);
-    const response = await fetch(
-      `https://dummyjson.com/products?limit=20&skip=${
-        count === 0 ? 0 : count * 20
-      }`
-    );
+  async function fetchProducts() {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `https://dummyjson.com/products?limit=20&skip=${
+          count === 0 ? 0 : count * 20
+        }`
+      );
 
-    const result = await response.json();
+      const result = await response.json();
 
-    if (result && result.products && result.products.length) {
-      setProducts((prevData) => [...prevData, ...result.products]);
+      if (result && result.products && result.products.length) {
+        setProducts((prevData) => [...prevData, ...result.products]);
+        setLoading(false);
+      }
+
+      console.log(result);
+    } catch (e) {
+      console.log(e);
       setLoading(false);
     }
-
-    console.log(result);
-  } catch (e) {
-    console.log(e);
-    setLoading(false);
   }
-}
 
+  // useeffect
+  useEffect(() => {
+    fetchProducts();
+  }, [count]);
 
+  useEffect(() => {
+    if (products && products.length === 100) setDisableButton(true);
+  }, [products]);
 
-// useeffect 
-useEffect(() => {
-  fetchProducts();
-}, [count]);
-
+  if (loading) {
+    return <div>Loading data ! Please wait.</div>;
+  }
 
   return (
     <>
       <div className=""></div>
     </>
-  )
-}
+  );
+};
 
-export default LoadMoreData
-
+export default LoadMoreData;
 
 // this is the website where we can get dummy products for this project
 // dummyjson.com/docs/products
