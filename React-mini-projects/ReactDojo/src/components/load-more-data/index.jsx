7af9467acb811b7a7437e47a417478145acba206
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-12-13 19:42:13 +0100

    chore: add useStates and reference link for dummy products data

--- React-mini-projects/ReactDojo/src/components/load-more-data/index.jsx
+++ React-mini-projects/ReactDojo/src/components/load-more-data/index.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react"
 
 
 const LoadMoreData = () => {
+
+  // usestates
+const [loading, setLoading] = useState(false);
+const [products, setProducts] = useState([]);
+const [count, setCount] = useState(0);
+
+
   return (
     <>
-    <div className="">LoadMoreData</div>
+      <div className=""></div>
     </>
   )
 }
 
 export default LoadMoreData
+
+
+// this is the website where we can get dummy products for this project
+// dummyjson.com/docs/products
