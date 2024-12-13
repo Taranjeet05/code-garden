63c2dbd5ab7a91e36ca64525d1adad30db7348ef
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-12-13 19:45:29 +0100

    feat: add fetchProducts function for loading and updating product data

--- React-mini-projects/ReactDojo/src/components/load-more-data/index.jsx
+++ React-mini-projects/ReactDojo/src/components/load-more-data/index.jsx
@@ -8,6 +8,30 @@ const [loading, setLoading] = useState(false);
 const [products, setProducts] = useState([]);
 const [count, setCount] = useState(0);
 
+async function fetchProducts() {
+  try {
+    setLoading(true);
+    const response = await fetch(
+      `https://dummyjson.com/products?limit=20&skip=${
+        count === 0 ? 0 : count * 20
+      }`
+    );
+
+    const result = await response.json();
+
+    if (result && result.products && result.products.length) {
+      setProducts((prevData) => [...prevData, ...result.products]);
+      setLoading(false);
+    }
+
+    console.log(result);
+  } catch (e) {
+    console.log(e);
+    setLoading(false);
+  }
+}
+
+
 
 // useeffect 
 useEffect(() => {
