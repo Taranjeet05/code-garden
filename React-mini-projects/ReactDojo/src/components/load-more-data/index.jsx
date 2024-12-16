e18328a6630557b1c909b7d268e49e1d83800a21
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-12-16 05:14:25 +0100

    feat: render products dynamically with thumbnail and title

--- React-mini-projects/ReactDojo/src/components/load-more-data/index.jsx
+++ React-mini-projects/ReactDojo/src/components/load-more-data/index.jsx
@@ -45,7 +45,16 @@ const LoadMoreData = () => {
 
   return (
     <>
-      <div className=""></div>
+      <div className="product-container">
+        {products && products.length
+          ? products.map((item) => (
+              <div className="product" key={item.id}>
+                <img src={item.thumbnail} alt={item.title} />
+                <p>{item.title}</p>
+              </div>
+            ))
+          : null}
+      </div>
     </>
   );
 };
