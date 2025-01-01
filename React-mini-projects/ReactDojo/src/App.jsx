a9c8059ddb70ffe78daecc6454a9f271448c538b
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-01-01 17:58:20 +0100

    Uncommented all components in App.jsx

--- React-mini-projects/ReactDojo/src/App.jsx
+++ React-mini-projects/ReactDojo/src/App.jsx
@@ -1,26 +1,26 @@
 import "./App.css";
 import ImageSlider from "./components/image-slider";
-//import LoadMoreData from "./components/load-more-data";
-//import Accordion from "./components/accordion";
-//import RandomColor from "./components/random-color";
-//import StarRating from "./components/star-rating";
+import LoadMoreData from "./components/load-more-data";
+import Accordion from "./components/accordion";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 
 function App() {
   return (
     <>
       {/* Accordion */}
-      {/*<Accordion />*/}
+      <Accordion />
       {/*Random-Color */}
-      {/* <RandomColor /> /*}
+      <RandomColor />
       {/*Star-Rating*/}
-      {/*<StarRating noOfStars={5} starSize={60} />*/}
+      <StarRating noOfStars={5} starSize={60} />
       {/* Image-Slider */}
       <ImageSlider
         url={"https://picsum.photos/v2/list"}
         page={"1"}
         limit={"10"}
       />
-      {/*<LoadMoreData />*/}
+      <LoadMoreData />
     </>
   );
 }
