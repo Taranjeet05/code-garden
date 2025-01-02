edb34093d5c5b11c493fc2ee87972416c0335222
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-01-02 18:01:27 +0100

    Add support for rendering nested menu items in MenuItem component

--- React-mini-projects/ReactDojo/src/components/tree-view/menu-item.jsx
+++ React-mini-projects/ReactDojo/src/components/tree-view/menu-item.jsx
@@ -1,9 +1,13 @@
 import PropTypes from "prop-types";
+import MenuList from "./menu-list";
 
 const MenuItem = ({ item }) => {
   return (
     <li>
       <p>{item.label}</p>
+      {item && item.children && item.children.length > 0 ?
+      <MenuList list={item.children} />
+      : null}
     </li>
   );
 };
