62e2e320b8f3cf449d1cb86a8bb373d565f3c922
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-01-23 18:00:26 +0100

    Update Home component to enhance layout and styling with new content

--- Tailwind-CSS-Project/tailwind-play/app/page.js
+++ Tailwind-CSS-Project/tailwind-play/app/page.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div className="text-lg text-center text-blue-400">Hello World</div>
+      <div className="w-full h-10 bg-violet-200 border-violet-600 p-2 rounded-md my-4">
+        <p className="text-center font-mono font-extrabold">
+          This is a div with p tag.
+        </p>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
