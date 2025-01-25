60fa5382da9299ba20ff70ddb73aa8073ae14c94
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-01-25 18:43:07 +0100

    Refactor Home component structure and update naming conventions

--- Tailwind-CSS-Project/nike-landing-page/app/page.js
+++ Tailwind-CSS-Project/nike-landing-page/app/page.js
@@ -1,13 +1,17 @@
-const page = () => {
+import React from "react";
+
+const Home = () => {
   return (
-    <div className="container mx-auto space-y-7 mt-3">
-      <p className="text-2xl font-mono font-bold text-center">
-        This is my Home Page, Welcome!!
-      </p>
-      <p class="font-Palanquin text-2xl font-bold">This text uses the Palanquin font.</p>
-      <p class="font-Montserrat text-2xl font-bold">This text uses the Montserrat font.</p>
-    </div>
+   <>
+   <main>
+    {/*hero section*/}
+    {/*PopularProducts*/}
+    {/*SuperQuality*/}
+    {/*Services */}
+    {/*specialOffers*/}
+   </main>
+   </>
   );
 };
 
-export default page;
+export default Home;
