3142e8ded57edb33f881c2550887c0c22bd2e075
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-01-26 19:27:46 +0100

    Update Home component to structure main layout and add content sections

--- Tailwind-CSS-Project/nike-landing-page/app/page.js
+++ Tailwind-CSS-Project/nike-landing-page/app/page.js
@@ -2,15 +2,29 @@ import React from "react";
 
 const Home = () => {
   return (
-   <>
-   <main>
-    {/*hero section*/}
-    {/*PopularProducts*/}
-    {/*SuperQuality*/}
-    {/*Services */}
-    {/*specialOffers*/}
-   </main>
-   </>
+    <>
+      <main className="relative">
+        Navbar
+        {/*hero section*/}
+        <section className="xl:padding-1 wide:padding-r padding-b">
+          Hero
+        </section>
+        {/*PopularProducts*/}
+        <section className="padding">Popular Products</section>
+        {/*SuperQuality*/}
+        <section className="padding">Super Quality</section>
+        {/*Services */}
+        <section className="padding-x py-10">Services</section>
+        {/*specialOffers*/}
+        <section className="padding bg-pale-blue ">Special Offers</section>
+        {/* customer reviews */}
+        <section className="padding">customer reviews</section>
+        {/* subscribe */}
+        <section className="padding-x sm:py-32 py-16 w-full">subscribe</section>
+        {/* footer */}
+        <section className="padding-x padding-t pb-8 bg-black ">Footer</section>
+      </main>
+    </>
   );
 };
 
