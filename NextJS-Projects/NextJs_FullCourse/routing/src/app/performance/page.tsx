ab54a2380e91b912750095f86ef445466d550526
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-01-29 19:04:18 +0100

    Add Performance and Reliability pages with basic structure

---
+++ NextJS-Projects/NextJs_FullCourse/routing/src/app/performance/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PerformancePage = () => {
+  return (
+    <div>
+      <p>Hello I am Performance Page.</p>
+    </div>
+  )
+}
+
+export default PerformancePage
---
+++ NextJS-Projects/NextJs_FullCourse/routing/src/app/reliability/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ReliabilityPage = () => {
+  return (
+    <div>
+      <p>Hello I am Reliability Page</p>
+    </div>
+  )
+}
+
+export default ReliabilityPage
