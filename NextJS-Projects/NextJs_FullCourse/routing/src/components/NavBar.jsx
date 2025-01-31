14bc4e14d0686f97a323662e4c22f551effdd839
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-01-31 17:06:26 +0100

    Add NavBar component with navigation links for Home, Performance, and Reliability

--- NextJS-Projects/NextJs_FullCourse/routing/src/components/NavBar.jsx
+++ NextJS-Projects/NextJs_FullCourse/routing/src/components/NavBar.jsx
@@ -1,8 +1,30 @@
+import Link from "next/link";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <div className="flex items-center justify-between p-5 mx-auto border-b-2 border-green-900">
+      <Link
+        href="/"
+        className="text-2xl font-mono hover:text-slate-400 active:text-green-200"
+      >
+        <h1>Home</h1>
+      </Link>
+      <div className="flex gap-10">
+        <Link
+          href="/performance"
+          className="text-lg font-mono hover:text-slate-400 active:text-green-200"
+        >
+          Performance
+        </Link>
+        <Link
+          href="/reliabilty"
+          className="text-lg font-mono hover:text-slate-400 active:text-green-200"
+        >
+          Reliability
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
