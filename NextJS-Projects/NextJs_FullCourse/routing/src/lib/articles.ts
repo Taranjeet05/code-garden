ffe22331e406cc4ba96b74d4ed7f0b5ab847ddbd
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-01-31 18:02:58 +0100

    Add articles data and type definitions for Next.js routing features

---
+++ NextJS-Projects/NextJs_FullCourse/routing/src/lib/articles.ts
@@ -0,0 +1,19 @@
+// Here come the sample Data for the articles.
+
+export const articles = [
+  {
+    id: 1,
+    slug: "nextjs-routing",
+    title: "Next.js Routing",
+    content:
+      "Next.js uses a powerful file-based routing system that allows you to create routes with ease...",
+  },
+
+  {
+    id: 2,
+    slug: "server-components",
+    title: "Server Components in Next.js",
+    content:
+      "Server components are an exciting feature in Next.js that allow you to render parts of your app on the server...",
+  },
+];
