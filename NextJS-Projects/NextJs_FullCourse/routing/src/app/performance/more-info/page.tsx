2a098b66021571757971359d0374e734e5e7e22a
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-01-31 17:32:01 +0100

    Add Performance and Reliability more info pages with basic structure

---
+++ NextJS-Projects/NextJs_FullCourse/routing/src/app/performance/more-info/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PerformanceMoreInfoPage = () => {
+  return (
+    <div> MoreInfoPage</div>
+  )
+}
+
+export default PerformanceMoreInfoPage
---
+++ NextJS-Projects/NextJs_FullCourse/routing/src/app/reliability/more-info/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ReliabilityMoreInfoPage = () => {
+  return (
+    <div>ReliabilityMoreInfoPage</div>
+  )
+}
+
+export default ReliabilityMoreInfoPage
