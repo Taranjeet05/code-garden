e5a765a7aa44975cb85951253c6dea832f7c8d51
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-02-02 19:49:00 +0100

    Refactor Home page layout for improved readability and consistency

--- NextJS-Projects/NextJs_FullCourse/snippets/src/app/page.tsx
+++ NextJS-Projects/NextJs_FullCourse/snippets/src/app/page.tsx
@@ -4,30 +4,30 @@ import Link from "next/link";
 import React from "react";
 
 const Home = async () => {
-
   const snippets = await prisma.snippet.findMany();
 
   return (
     <div className="">
-      <Link href='/'>
+      <Link href="/">
         <h1 className="text-5xl font-mono font-bold">Home</h1>
       </Link>
       <div className="flex items-center justify-between">
         <h1>Snippets</h1>
-        <Link href='/snippet/new'>
-        <Button>New</Button>
+        <Link href="/snippet/new">
+          <Button>New</Button>
         </Link>
       </div>
-      {
-        snippets.map((snippet) => (
-          <div key={snippet.id} className="bg-green-500 flex items-center justify-between mt-5">
-            <h1>{snippet.title}</h1>
-            <Link href={`/snippet/${snippet.id}`}>
+      {snippets.map((snippet) => (
+        <div
+          key={snippet.id}
+          className="bg-green-500 flex items-center justify-between mt-5"
+        >
+          <h1>{snippet.title}</h1>
+          <Link href={`/snippet/${snippet.id}`}>
             <Button>View</Button>
-            </Link>
-          </div>
-        ))
-      }
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
