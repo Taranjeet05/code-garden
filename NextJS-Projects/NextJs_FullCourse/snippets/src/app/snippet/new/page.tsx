c48934d8d6bd110014c8e5736a4f7627cd7cfa1f
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-02-02 17:51:06 +0100

    Add Prisma integration for snippet creation and update package.json dependencies

--- NextJS-Projects/NextJs_FullCourse/snippets/src/app/snippet/new/page.tsx
+++ NextJS-Projects/NextJs_FullCourse/snippets/src/app/snippet/new/page.tsx
@@ -2,14 +2,32 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import { prisma } from "@/lib/prisma";
+import { redirect } from "next/navigation";
 import React from "react";
 
 const CreateSnippetPage = () => {
+  async function CreateSnippet(formData: FormData) {
+    "use server"; // use server directive
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+
+    const snippet = await prisma.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+    console.log("Created Snippet", snippet);
+
+    redirect("/");
+  }
+
   return (
-    <form>
+    <form action={CreateSnippet}>
       <div>
         <Label>Title</Label>
-        <Input type="text" name="Title" id="title" />
+        <Input type="text" name="title" id="title" />
       </div>
       <div>
         <Label>Code</Label>
