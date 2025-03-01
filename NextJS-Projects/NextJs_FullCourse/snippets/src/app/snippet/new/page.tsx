7dd598b259dc242a34f35629576052236a68463a
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-01 19:15:14 +0100

    Add form state handling and error message display in CreateSnippetPage

--- NextJS-Projects/NextJs_FullCourse/snippets/src/app/snippet/new/page.tsx
+++ NextJS-Projects/NextJs_FullCourse/snippets/src/app/snippet/new/page.tsx
@@ -1,15 +1,16 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { CreateSnippet } from "@/actions/index";
+import { useActionState } from "react";
 
 const CreateSnippetPage = () => {
-
-  
+  const [formStateData, xyz] = useActionState(CreateSnippet, { message: "" });
 
   return (
-    <form action={CreateSnippet}>
+    <form action={xyz}>
       <div>
         <Label>Title</Label>
         <Input type="text" name="title" id="title" />
@@ -21,6 +22,15 @@ const CreateSnippetPage = () => {
           name="code"
           id="code"
         />
+      </div>
+      <div
+        className={`${
+          formStateData.message
+            ? "text-red-600 bg-red-100 border border-red-400 p-2 rounded-md mt-3 text-sm animate-fade-in duration-300 opacity-100"
+            : "hidden"
+        }`}
+      >
+        {formStateData.message}
       </div>
       <Button type="submit" className="my-4">
         New
