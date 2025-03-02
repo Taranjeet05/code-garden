60d7213a836ed91ba0faea54cdd1d7b29e218829
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-02 18:45:50 +0100

    Add Prisma integration and update home page with a button component

--- NextJS-Projects/NextJs_FullCourse/discuss/src/app/page.tsx
+++ NextJS-Projects/NextJs_FullCourse/discuss/src/app/page.tsx
@@ -1,6 +1,11 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
+    <>
     <h1 className="text-5xl text-center font-mono">Hello, Welcome to the Home page /.</h1>
+    <Button>Let&apos;s build discuss app</Button>
+    </>
+    
   );
 }
