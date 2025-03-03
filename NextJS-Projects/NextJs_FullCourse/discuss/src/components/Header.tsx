808f171dfee548a7abd685b651aac2fa4f1c6ceb
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-03 19:19:05 +0100

    Refactor Header component to include search input and GitHub sign-in/sign-up buttons

--- NextJS-Projects/NextJs_FullCourse/discuss/src/components/Header.tsx
+++ NextJS-Projects/NextJs_FullCourse/discuss/src/components/Header.tsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Input } from "./ui/input";
+import { signIn } from "@/app/actions/sign-in";
+import { Button } from "@/components/ui/button";
 
-const Header = () => {
+
+const HeaderPage = () => {
   return (
     <div>
       <div>
         <h1>Discuss</h1>
       </div>
-      <div></div>
-      <div></div>
+      <div>
+        <Input type="text" placeholder="Search post ..." />
+      </div>
+      <div>
+      <form action={signIn}>
+            <Button variant={"outline"} type="submit">SignIn with GITHUB</Button>
+          </form>
+          <form action={signIn}>
+            <Button type="submit">SignUp</Button>
+          </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default HeaderPage;
