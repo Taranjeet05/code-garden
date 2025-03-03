ac609e34478ea67c6c416af95725e1d940c4494d
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-03 19:33:40 +0100

    **Refactor Home component: Removed authentication forms and session handling**

--- NextJS-Projects/NextJs_FullCourse/discuss/src/app/page.tsx
+++ NextJS-Projects/NextJs_FullCourse/discuss/src/app/page.tsx
@@ -1,27 +1,10 @@
-import { Button } from "@/components/ui/button";
-import { signIn } from "@/app/actions/sign-in";
-import { signOut } from "@/app/actions/sign-out";
-import { auth } from "@/auth";
 
 export default async function Home() {
-  const session = await auth();
+
 
   return (
     <>
-      <div className="">
         <h1 className="text-5xl text-center font-mono">Home Page</h1>
-        <div className="flex justify-start space-x-4 m-7">
-          <form action={signIn}>
-            <Button type="submit">SignIn with GITHUB</Button>
-          </form>
-          <form action={signOut}>
-            <Button type="submit">SignOut</Button>
-          </form>
-        </div>
-        {
-          session?.user && <div>{JSON.stringify(session.user)}</div>
-        }
-      </div>
     </>
   );
 }
