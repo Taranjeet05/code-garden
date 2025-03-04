c722db42aaf1bd0191ac02c0ae1154539c575f7d
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-04 19:38:24 +0100

    Update page.tsx

--- NextJS-Projects/NextJs_FullCourse/discuss/src/app/page.tsx
+++ NextJS-Projects/NextJs_FullCourse/discuss/src/app/page.tsx
@@ -1,10 +1,18 @@
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
-
-
   return (
     <>
-        <h1 className="text-5xl text-center font-mono">Home Page</h1>
+      <div className="grid grid-cols-4 gap-4 p-4">
+        <div className="col-span-3">
+          <h1 className="text-5xl text-center font-mono m-2 font-bold">
+            Home Page
+          </h1>
+        </div>
+        <div className="col-span-1">
+          <Button>New Topic</Button>
+        </div>
+      </div>
     </>
   );
 }
