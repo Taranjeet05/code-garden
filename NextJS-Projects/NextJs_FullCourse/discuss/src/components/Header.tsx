b5d33e588850cba02f45fe330d662b128141dd26
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-04 17:29:31 +0100

    Add Popover & Separator components; update Header with user session

--- NextJS-Projects/NextJs_FullCourse/discuss/src/components/Header.tsx
+++ NextJS-Projects/NextJs_FullCourse/discuss/src/components/Header.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { Input } from "./ui/input";
 import { signIn } from "@/app/actions/sign-in";
+import { signOut } from "@/app/actions/sign-out";
 import { Button } from "@/components/ui/button";
+import { auth } from "@/auth";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { LogOutIcon } from "lucide-react";
+import { Separator } from "./ui/separator";
 
-
-const HeaderPage = () => {
+const HeaderPage = async () => {
+  const session = await auth();
   return (
     <div className="grid grid-cols-3 h-14 items-center">
       <div className="flex justify-start">
@@ -14,12 +24,35 @@ const HeaderPage = () => {
         <Input type="text" placeholder="Search post ..." />
       </div>
       <div className="flex justify-end gap-2">
-      <form action={signIn}>
-            <Button variant={"outline"} type="submit">SignIn with GITHUB</Button>
-          </form>
-          <form action={signIn}>
-            <Button type="submit">SignUp</Button>
-          </form>
+        {session?.user ? (
+          <Popover>
+            <PopoverTrigger asChild>
+              <Avatar>
+                <AvatarImage src={session.user.image || ""} alt="@shadcn" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </PopoverTrigger>
+            <PopoverContent>
+              <h1 className="font-bold font-mono">{session.user.name}</h1>
+              <h1 className="font-medium font-serif">{session.user.email}</h1>
+              <Separator className="my-2" />
+              <form action={signOut}>
+                <Button>
+                  <LogOutIcon /> LogOut
+                </Button>
+              </form>
+            </PopoverContent>
+          </Popover>
+        ) : (
+          <>
+            <form action={signIn}>
+              <Button variant={"outline"} type="submit">
+                SignIn with GITHUB
+              </Button>
+              <Button type="submit">SignUp</Button>
+            </form>
+          </>
+        )}
       </div>
     </div>
   );
