284a9e8b7fa23698bd07c3226e46514a3c0320fc
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-04 21:11:00 +0100

    Add Textarea component to TopicCreateForm for improved input handling

--- NextJS-Projects/NextJs_FullCourse/discuss/src/components/topics/TopicCreateForm.tsx
+++ NextJS-Projects/NextJs_FullCourse/discuss/src/components/topics/TopicCreateForm.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Textarea } from "../ui/textarea";
 
 const TopicCreateForm = () => {
   return (
@@ -21,25 +22,24 @@ const TopicCreateForm = () => {
         <DialogHeader>
           <DialogTitle>Create a Topic</DialogTitle>
           <DialogDescription>
-            Write a new topic to start discussion. Click save when you&apos;re done.
+            Write a new Topic to start discussion. Click save when you&apos;re
+            done.
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+          <div>
+            <Label htmlFor="name">Name</Label>
+            <Input id="name" name="name" />
           </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
+          <div>
+            <Label htmlFor="discription" className="text-right">
+              Discription
             </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
+            <Textarea id="discription" name="discription" />
           </div>
         </div>
         <DialogFooter>
-          <Button type="submit">Save changes</Button>
+          <Button type="submit" className="w-full">Save changes</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
