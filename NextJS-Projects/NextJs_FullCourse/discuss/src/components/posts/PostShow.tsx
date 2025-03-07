5534502763008ae8de1c847012652d3e033953a3
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-07 19:00:29 +0100

    Enhance PostShow component: Fetch post details from database and handle not found case

--- NextJS-Projects/NextJs_FullCourse/discuss/src/components/posts/PostShow.tsx
+++ NextJS-Projects/NextJs_FullCourse/discuss/src/components/posts/PostShow.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { prisma } from "@/lib";
 
 type PostShowProps = {
   postId: string;
 };
 
 const PostShow = async ({ postId }: PostShowProps) => {
-  return <div>PostShow</div>;
+  const post = await prisma.post.findUnique({
+    where: {
+      id: postId,
+    },
+  });
+
+  if (!post) {
+    return <div>Post not found</div>;
+  }
+  return (
+    <div>
+      <h1>Post Show Page - {postId}</h1>
+    </div>
+  );
 };
 
 export default PostShow;
