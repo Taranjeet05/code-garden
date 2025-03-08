4ebb04f6666c9a770af74ef98c1c6d2b0d170b56
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-08 16:15:52 +0100

    Add CommentCreateForm to PostShowPage for user comments

--- NextJS-Projects/NextJs_FullCourse/discuss/src/app/topics/[slug]/posts/[postid]/page.tsx
+++ NextJS-Projects/NextJs_FullCourse/discuss/src/app/topics/[slug]/posts/[postid]/page.tsx
@@ -1,3 +1,4 @@
+import CommentCreateForm from "@/components/comments/CommentCreateForm";
 import PostShow from "@/components/posts/PostShow";
 import React from "react";
 
@@ -13,6 +14,7 @@ const PostShowPage = async ({params}:PostShowPageProps)  => {
   return (
     <div>
       <PostShow postId={postid} />
+      <CommentCreateForm />
     </div>
   )
 };
--- NextJS-Projects/NextJs_FullCourse/discuss/src/components/comments/CommentCreateForm.tsx
+++ NextJS-Projects/NextJs_FullCourse/discuss/src/components/comments/CommentCreateForm.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Button } from "../ui/button";
+import { Textarea } from "../ui/textarea";
 
 const CommentCreateForm = () => {
   return (
-    <div>CommentCreateForm</div>
-  )
-}
+    <div>
+      <Button size={"sm"} variant={"link"}>
+        Reply
+      </Button>
+      <form action="" className="space-y-2">
+        <Textarea
+          placeholder="Write a comment..."
+          className="bg-gray-100 focus-visible:ring-0 focus:ring-0 focus:ring-transparent"
+        />
+        <Button size={"sm"} variant={"secondary"} type="submit">
+          save
+        </Button>
+      </form>
+    </div>
+  );
+};
 
-export default CommentCreateForm
+export default CommentCreateForm;
