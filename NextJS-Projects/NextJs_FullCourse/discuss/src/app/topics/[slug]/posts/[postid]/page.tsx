6ce69bba758058c1f7182cbfead535eb5643c213
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-08 16:22:04 +0100

    Enhance PostShowPage: Add back navigation and improve layout

--- NextJS-Projects/NextJs_FullCourse/discuss/src/app/topics/[slug]/posts/[postid]/page.tsx
+++ NextJS-Projects/NextJs_FullCourse/discuss/src/app/topics/[slug]/posts/[postid]/page.tsx
@@ -1,22 +1,29 @@
 import CommentCreateForm from "@/components/comments/CommentCreateForm";
 import PostShow from "@/components/posts/PostShow";
+import { Button } from "@/components/ui/button";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
 import React from "react";
-
 
 type PostShowPageProps = {
   params: Promise<{ slug: string; postid: string }>;
 };
 
-const PostShowPage = async ({params}:PostShowPageProps)  => {
-
+const PostShowPage = async ({ params }: PostShowPageProps) => {
   const { slug, postid } = await params;
 
   return (
-    <div>
+    <div className="space-y-3">
+      <Link href={`/topics/${slug}`}>
+        <Button variant={"link"}>
+          <ChevronLeft className="w-6 h-6" />
+          Back to {slug}
+        </Button>
+      </Link>
       <PostShow postId={postid} />
       <CommentCreateForm />
     </div>
-  )
+  );
 };
 
 export default PostShowPage;
