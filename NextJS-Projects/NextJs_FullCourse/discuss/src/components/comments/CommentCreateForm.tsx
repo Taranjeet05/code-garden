9d77444162220219408f856329447425a93684b6
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-08 16:30:20 +0100

    Enhance CommentCreateForm: Add toggle functionality for comment input

--- NextJS-Projects/NextJs_FullCourse/discuss/src/components/comments/CommentCreateForm.tsx
+++ NextJS-Projects/NextJs_FullCourse/discuss/src/components/comments/CommentCreateForm.tsx
@@ -1,22 +1,27 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
 
 const CommentCreateForm = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div>
-      <Button size={"sm"} variant={"link"}>
+      <Button size={"sm"} variant={"link"} onClick={() => setOpen(!open)}>
         Reply
       </Button>
-      <form action="" className="space-y-2">
-        <Textarea
-          placeholder="Write a comment..."
-          className="bg-gray-100 focus-visible:ring-0 focus:ring-0 focus:ring-transparent"
-        />
-        <Button size={"sm"} variant={"secondary"} type="submit">
-          save
-        </Button>
-      </form>
+      {open && (
+        <form action="" className="space-y-2">
+          <Textarea
+            placeholder="Write a comment..."
+            className="bg-gray-100 focus-visible:ring-0 focus:ring-0 focus:ring-transparent"
+          />
+          <Button size={"sm"} variant={"secondary"} type="submit">
+            save
+          </Button>
+        </form>
+      )}
     </div>
   );
 };
