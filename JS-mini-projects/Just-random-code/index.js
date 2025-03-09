8e59c575c1265a1ebb4e81b65d5a925cc07cbb2b
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-09 21:41:34 +0100

    feat: Add function to remove every second element from an array

--- JS-mini-projects/Just-random-code/index.js
+++ JS-mini-projects/Just-random-code/index.js
@@ -8,7 +8,19 @@ prepended by the correct number.
 The numbering starts at 1. The format is n: string. Notice the colon and space in between.
 */
 
-var number=function(array){
+var number = function (array) {
   //your awesome code here
-  return array.map((line, index)=> `${index + 1 }: ${line}`)
+  return array.map((line, index) => `${index + 1}: ${line}`);
+};
+
+/* Take an array and remove every second element from the array. Always keep 
+the first element and start removing with the next element.
+
+Example:
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that! */
+
+function removeEveryOther(arr){
+  //your code here
 }
