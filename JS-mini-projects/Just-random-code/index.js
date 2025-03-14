a5d2679778e022faa9a456a07d4712cfd8cf677c
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-14 21:29:26 +0100

    feat: Add factorial function to calculate the factorial of a number

--- JS-mini-projects/Just-random-code/index.js
+++ JS-mini-projects/Just-random-code/index.js
@@ -1,1 +1,16 @@
 // 7. Find the factorial of a number (e.g., 5!).
+
+function factorial(num) {
+  let result = 1;
+  let i = 1;
+
+  while (i <= num) {
+    result *= i;
+    i++;
+  }
+
+  return result;
+}
+
+// Example:
+console.log(factorial(5)); // Output: 120
