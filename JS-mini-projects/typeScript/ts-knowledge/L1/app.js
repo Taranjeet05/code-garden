6c39c70cd3dbe9eb3ddebb015f723e22409c2728
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-16 00:02:05 +0100

    fix: Add type annotations to string variables in app.js

--- JS-mini-projects/typeScript/ts-knowledge/L1/app.js
+++ JS-mini-projects/typeScript/ts-knowledge/L1/app.js
@@ -11,7 +11,7 @@ function add(num1, num2, printResult, goodText, badText) {
 var n1 = 54; // number
 var n2 = 54; // number
 var printResult = true;
-var goodText = 'We let you see the result check console ✅';
-var badText = 'We are not let you see the result because your result is false.❌';
+var goodText = 'We let you see the result check console ✅'; // string
+var badText = 'We are not let you see the result because your result is false.❌'; // string
 var ans = add(n1, n2, printResult, goodText, badText);
 console.log(ans);
