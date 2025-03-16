f7a121ebd34c30fae9632c68cf5ec28a4dce5c71
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-16 20:54:43 +0100

    feat: Add student report card implementation with TypeScript types and update HTML title

--- JS-mini-projects/typeScript/ts-knowledge/L3/app.js
+++ JS-mini-projects/typeScript/ts-knowledge/L3/app.js
@@ -1,1 +1,28 @@
-console.log('Hello We are in L3 folder');
+// A Student report card
+// Assign type to student
+var student = {
+    studentId: 'Singg565',
+    name: 'Taranjeet Singh',
+    rollNumber: 11,
+    class: "10 Grade",
+    reports: [
+        {
+            subject: "Math",
+            grade: "A",
+            status: "Pass",
+        },
+        {
+            subject: "Science",
+            grade: "B",
+            status: "Pass",
+        },
+        {
+            subject: "English",
+            grade: "C",
+            status: "Pass",
+        },
+    ],
+};
+console.log("Student ID:", student.studentId);
+console.log(student.name, student.class, student.rollNumber);
+console.log(student.reports[0], student.reports[1], student.reports[2]);
--- JS-mini-projects/typeScript/ts-knowledge/L3/app.ts
+++ JS-mini-projects/typeScript/ts-knowledge/L3/app.ts
@@ -1,1 +1,51 @@
-console.log('Hello We are in L3 folder');
+// A Student report card
+
+// Literal Types using Union
+type Subject = "Math" | "Science" | "English";
+type Status = "Pass" | "Fail";
+type Grade = "A" | "B" | "C" | "D" | "E" | "F";
+
+// Type Alias
+type Reports = {
+  subject: Subject;
+  grade: Grade;
+  status: Status;
+};
+
+// Type Alias + Union Type
+type Student = {
+  studentId: number | string;
+  name: string;
+  rollNumber: number;
+  class: string;
+  reports: Reports[];
+};
+
+// Assign type to student
+const student: Student = {
+  studentId: 'Singg565' ,
+  name: 'Taranjeet Singh',
+  rollNumber: 11,
+  class: "10 Grade",
+  reports: [
+    {
+      subject: "Math",
+      grade: "A",
+      status: "Pass",
+    },
+    {
+      subject: "Science",
+      grade: "B",
+      status: "Pass",
+    },
+    {
+      subject: "English",
+      grade: "C",
+      status: "Pass",
+    },
+  ],
+};
+
+console.log(`Student ID:`,student.studentId);
+console.log(student.name , student.class, student.rollNumber);
+console.log(student.reports[0], student.reports[1], student.reports[2])
