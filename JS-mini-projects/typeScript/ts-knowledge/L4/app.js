2f36a5f84cde468d8bb07d3aa21592fa9048311c
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-18 20:05:15 +0100

    feat: Add function return types and callback examples in app.js and app.ts

--- JS-mini-projects/typeScript/ts-knowledge/L4/app.js
+++ JS-mini-projects/typeScript/ts-knowledge/L4/app.js
@@ -1,1 +1,22 @@
-console.log('we are in L4 folder');
+//                             Function Return Type
+function add(num1, num2) {
+    return num1 + num2;
+}
+console.log(add(10, 50));
+function greeting(name) {
+    console.log("Hello ".concat(name));
+}
+greeting('Taranjeet Singh');
+// let's practice Callback function::
+function doSomething(callback) {
+    callback('Hello, This message is from callback that honesty is the best policy');
+}
+doSomething(function (message) {
+    console.log("Here is the message from callback function it says : ".concat(message));
+});
+//                         💡 IDEAS YOU CAN CODE YOURSELF
+// 💬 1. Chat App Simulation
+function sendMessage(callback) {
+    callback('Hello Chat app Simulation');
+}
+sendMessage(function (message) { return console.log("here is you message >> ".concat(message)); });
--- JS-mini-projects/typeScript/ts-knowledge/L4/app.ts
+++ JS-mini-projects/typeScript/ts-knowledge/L4/app.ts
@@ -1,1 +1,33 @@
-console.log('we are in L4 folder');
+//                             Function Return Type
+
+function add (num1 : number, num2: number) : number  {
+  return num1 + num2; 
+}
+
+console.log(add(10, 50));
+
+function greeting (name : string) : void {
+  console.log(`Hello ${name}`);
+}
+
+greeting('Taranjeet Singh');
+
+// let's practice Callback function::
+
+function doSomething (callback : (message : string) => void) :void {
+  callback('Hello, This message is from callback that honesty is the best policy')
+}
+
+doSomething((message) => {
+  console.log(`Here is the message from callback function it says : ${message}`)
+})
+
+//                         💡 IDEAS YOU CAN CODE YOURSELF
+
+// 💬 1. Chat App Simulation
+
+function sendMessage(callback : (message : string) => void) : void {
+  callback('Hello Chat app Simulation')
+}
+
+sendMessage((message) => console.log(`here is you message >> ${message}`));
