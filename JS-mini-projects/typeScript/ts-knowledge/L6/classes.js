c8379eb7b1944a3deaa9de924df92c54ced18c92
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-19 23:24:25 +0100

    feat: Add Student class with introduction method in classes.js and classes.ts

--- JS-mini-projects/typeScript/ts-knowledge/L6/classes.js
+++ JS-mini-projects/typeScript/ts-knowledge/L6/classes.js
@@ -1,1 +1,12 @@
-console.log('This is classes');
+"use strict";
+class Student {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    introduce() {
+        console.log(`Hi, I'm ${this.name} and I'm ${this.age} years old.`);
+    }
+}
+const taran = new Student("Taranjeet Singh", 20);
+taran.introduce();
--- JS-mini-projects/typeScript/ts-knowledge/L6/classes.ts
+++ JS-mini-projects/typeScript/ts-knowledge/L6/classes.ts
@@ -1,1 +1,16 @@
-console.log('This is classes');
+class Student {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number){
+    this.name = name;
+    this.age = age;
+  }
+
+  introduce(): void {
+    console.log(`Hi, I'm ${this.name} and I'm ${this.age} years old.`);
+  }
+}
+
+const taran = new Student("Taranjeet Singh", 20);
+taran.introduce();
