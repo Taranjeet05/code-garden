0509bb93acc8c4dccf7506f2136c8420c5bd9048
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-19 23:25:24 +0100

    feat: Add Human class with speak method in interface.js and interface.ts

--- JS-mini-projects/typeScript/ts-knowledge/L6/interface.js
+++ JS-mini-projects/typeScript/ts-knowledge/L6/interface.js
@@ -1,1 +1,12 @@
-console.log('This is interface');
+"use strict";
+class Human {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    speak() {
+        console.log("Hello! I can speak.");
+    }
+}
+const singh = new Human('Singh', 20);
+singh.speak();
--- JS-mini-projects/typeScript/ts-knowledge/L6/interface.ts
+++ JS-mini-projects/typeScript/ts-knowledge/L6/interface.ts
@@ -1,1 +1,22 @@
-console.log('This is interface')
+interface Person {
+  name: string;
+  age: number;
+  speak(): void;
+}
+
+class Human implements Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  speak() {
+    console.log("Hello! I can speak.");
+  }
+}
+
+const singh = new Human('Singh', 20);
+singh.speak();
