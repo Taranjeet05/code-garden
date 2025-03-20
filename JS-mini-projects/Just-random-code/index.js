3238a49d6711f3f8a1b24486e6529090604f937d
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-20 18:31:29 +0100

    feat: Add reverseString function to reverse input strings in index.js

--- JS-mini-projects/Just-random-code/index.js
+++ JS-mini-projects/Just-random-code/index.js
@@ -137,3 +137,9 @@ Tip: Try not to use the built-in methods like .map(), .filter(), or .reduce() in
 
 
 */
+
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+console.log(reverseString("hello")); // Output: "olleh"
