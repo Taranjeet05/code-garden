7b371dd0909caae6a79344d5bf93691ae7a5d924
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-20 18:38:16 +0100

    feat: Add removeDuplicates function to filter out duplicate values from an array in index.js

--- JS-mini-projects/Just-random-code/index.js
+++ JS-mini-projects/Just-random-code/index.js
@@ -154,3 +154,8 @@ function findMaxNumber(arr) {
 }
 
 console.log(findMax([3, 9, 1, 5, 12])); // Output: 12
+
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // Output: [1, 2, 3, 4, 5]
