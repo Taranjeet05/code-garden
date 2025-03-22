c8eeb8fc87d86e2ed08375db35127c1addfb47fd
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-22 20:16:57 +0100

    feat: Add Result component to display age calculation results in Card

--- Tailwind-CSS-Project/age-calculator-app/src/components/Card.tsx
+++ Tailwind-CSS-Project/age-calculator-app/src/components/Card.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import InputFields from './InputFields'
 import Button from './Button'
+import Result from './Result'
 
 const Card = () => {
   return (
     <div>
       <InputFields />
       <Button />
+      <Result />
     </div>
   )
 }
--- Tailwind-CSS-Project/age-calculator-app/src/components/Result.tsx
+++ Tailwind-CSS-Project/age-calculator-app/src/components/Result.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
 
 const Result = () => {
   return (
-    <div>Result</div>
-  )
-}
+    <div className="mt-10 ml-11">
+      <h1
+        className="text-9xl font-bold"
+        style={{ color: "var(--color-purple)" }}
+      >
+        --
+        <span
+          className="tracking-tight"
+          style={{ color: "var(--color-off-black)" }}
+        >
+          years
+        </span>
+      </h1>
+      <h1
+        className="text-9xl font-bold"
+        style={{ color: "var(--color-purple)" }}
+      >
+        --
+        <span
+          className="tracking-tight"
+          style={{ color: "var(--color-off-black)" }}
+        >
+          months
+        </span>
+      </h1>
+      <h1
+        className="text-9xl font-bold"
+        style={{ color: "var(--color-purple)" }}
+      >
+        --
+        <span
+          className="tracking-tight"
+          style={{ color: "var(--color-off-black)" }}
+        >
+          days
+        </span>
+      </h1>
+    </div>
+  );
+};
 
-export default Result
+export default Result;
