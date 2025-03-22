05cdf466d1cf0b5a0171f63824aef0931b8ad6b4
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-22 17:51:08 +0100

    feat: Enhance Button component with arrow icon and integrate into Card component

--- Tailwind-CSS-Project/age-calculator-app/src/components/Button.tsx
+++ Tailwind-CSS-Project/age-calculator-app/src/components/Button.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const Button = () => {
   return (
-    <div>Button</div>
-  )
-}
+    <div className="relative mt-16">
+      <hr className="border-gray-200 border-1" />
+      <button className="absolute right-0 top-0 mt-4">
+        <Image
+          src="/assets/images/icon-arrow.svg"
+          alt="arrow-right"
+          width={40}
+          height={40}
+        />
+      </button>
+    </div>
+  );
+};
 
-export default Button
+export default Button;
--- Tailwind-CSS-Project/age-calculator-app/src/components/Card.tsx
+++ Tailwind-CSS-Project/age-calculator-app/src/components/Card.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import InputFields from './InputFields'
+import Button from './Button'
 
 const Card = () => {
   return (
     <div>
       <InputFields />
+      <Button />
     </div>
   )
 }
