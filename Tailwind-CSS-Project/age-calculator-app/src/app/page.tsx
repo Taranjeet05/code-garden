179b713fff9496bfa2f18239c6eb3e03a36e3615
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-22 13:56:59 +0100

    feat: Add Poppins font and update styles in age calculator app

--- Tailwind-CSS-Project/age-calculator-app/src/app/page.tsx
+++ Tailwind-CSS-Project/age-calculator-app/src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-center font-mono text-2xl">Hello, Testing...</h1>
+      <p className="text-center mt-5 font-bold bg-purple-400">Testig the font poppins.</p>
     </div>
   );
 }
