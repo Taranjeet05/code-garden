972f22580e725bc102550a412d17a755babe52c4
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-22 20:10:11 +0100

    feat: Update Button component to use CSS variable for background color

--- Tailwind-CSS-Project/age-calculator-app/src/components/Button.tsx
+++ Tailwind-CSS-Project/age-calculator-app/src/components/Button.tsx
@@ -6,7 +6,10 @@ const Button = () => {
   return (
     <div className="flex items-center mt-16 relative">
       <hr className="border-gray-200 border-1 w-6xl" />
-      <button className="rounded-full bg-purple-400 p-2 flex items-center justify-center ml-4 absolute right-7">
+      <button
+        className="rounded-full p-2 flex items-center justify-center ml-4 absolute right-7"
+        style={{ backgroundColor: "var(--color-purple)" }}
+      >
         <FaArrowDown className="w-30 h-30 text-white font-thin " />
       </button>
     </div>
