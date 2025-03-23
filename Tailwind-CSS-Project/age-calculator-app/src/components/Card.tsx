db48e05d00f630f8055d3c7c046dd368faa4b1cf
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-23 23:17:05 +0100

    feat: Integrate age calculation logic into Card component and update props for InputFields and Result

--- Tailwind-CSS-Project/age-calculator-app/src/components/Card.tsx
+++ Tailwind-CSS-Project/age-calculator-app/src/components/Card.tsx
@@ -1,16 +1,32 @@
-import React from 'react'
-import InputFields from './InputFields'
-import Button from './Button'
-import Result from './Result'
+'use client';
+import React, { useState } from "react";
+import InputFields from "./InputFields";
+import Button from "./Button";
+import Result from "./Result";
+import { AgeResult, BirthDate } from "@/types";
+import { calculateAge } from "@/lib/ageCalculator";
 
 const Card = () => {
+  const [birthDate, setBirthDate] = useState<BirthDate>({
+    day: 0,
+    month: 0,
+    year: 0,
+  });
+
+  const [result, setResult] = useState<AgeResult | null>(null);
+
+  const handleCalculate = () => {
+    const age = calculateAge(birthDate);
+    setResult(age);
+  };
+
   return (
     <div>
-      <InputFields />
-      <Button />
-      <Result />
+      <InputFields setBirthDate={setBirthDate} />
+      <Button onClick={handleCalculate} />
+      <Result result={result} />
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
