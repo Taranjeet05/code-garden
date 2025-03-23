af99df8a63628209155b855bc045b91276bb0e1c
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-23 23:18:37 +0100

    feat: Enhance Button component to accept onClick prop for better event handling

--- Tailwind-CSS-Project/age-calculator-app/src/components/Button.tsx
+++ Tailwind-CSS-Project/age-calculator-app/src/components/Button.tsx
@@ -2,11 +2,16 @@
 import React from "react";
 import { FaArrowDown } from "react-icons/fa";
 
-const Button = () => {
+type ButtonProps = {
+  onClick: () => void;
+}
+
+const Button = ({onClick}: ButtonProps) => {
   return (
     <div className="flex items-center mt-16 relative">
       <hr className="border-gray-200 border-1 w-6xl" />
       <button
+      onClick={onClick}
         className="rounded-full p-2 flex items-center justify-center ml-4 absolute right-7"
         style={{ backgroundColor: "var(--color-purple)" }}
       >
