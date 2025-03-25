eb368f748d9ec7ebfb01d8b24dda8a68a319770a
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-25 21:49:00 +0100

    feat: Improve Button component styling for enhanced responsiveness and visual appeal

--- Tailwind-CSS-Project/age-calculator-app/src/components/Button.tsx
+++ Tailwind-CSS-Project/age-calculator-app/src/components/Button.tsx
@@ -9,16 +9,16 @@ type ButtonProps = {
 const Button = ({ onClick }: ButtonProps) => {
   return (
     <div className="flex items-center mt-8 md:mt-12 lg:mt-16 relative px-2 sm:px-4 md:ml-6 lg:ml-10">
-      <hr className="border-gray-200 border-1 w-6xl" />
+      <hr className="border-gray-200 border-1 w-6xl " />
       <button
         onClick={onClick}
-        className="rounded-full p-3 md:p-4 flex items-center justify-center absolute 
+        className="rounded-full p-3 md:p-4 lg:p-11 flex items-center justify-center absolute 
                  left-1/2 -translate-x-1/2 
-                 lg:left-auto lg:right-0 lg:-translate-x-0
-                 transform transition-all duration-300 hover:bg-black hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-600"
+                 lg:left-auto lg:right-4 lg:-translate-x-0
+                 transform transition-all duration-300 hover:bg-black hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-600 cursor-pointer"
         style={{ backgroundColor: "var(--color-purple)" }}
       >
-        <FaArrowDown className="w-4 h-4 md:w-5 md:h-5 lg:w-6 lg:h-6 text-white font-thin" />
+        <FaArrowDown className="w-4 h-4 md:w-5 md:h-5 lg:w-10 lg:h-10 text-white font-thin" />
       </button>
     </div>
   );
