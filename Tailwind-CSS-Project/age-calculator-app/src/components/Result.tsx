9aa9eb870a373d4e8972b67a99f80b03dcf0a2ec
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-25 17:07:40 +0100

    feat: Enhance Result component with improved styling for better readability and responsiveness

--- Tailwind-CSS-Project/age-calculator-app/src/components/Result.tsx
+++ Tailwind-CSS-Project/age-calculator-app/src/components/Result.tsx
@@ -10,38 +10,38 @@ const Result = ({ result }: ResultProps) => {
     value !== undefined ? value : "--";
 
   return (
-    <div className="mt-10 ml-11">
+    <div className="mt-8 md:mt-10 px-2 sm:px-4 md:ml-6 lg:ml-10 animated-slide">
       <h1
-        className="text-9xl font-bold"
+        className="text-4xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-9xl font-extrabold italic"
         style={{ color: "var(--color-purple)" }}
       >
         {format(result?.years)}
         <span
-          className="tracking-tight"
+          className="ml-2 tracking-tight italic"
           style={{ color: "var(--color-off-black)" }}
         >
           years
         </span>
       </h1>
       <h1
-        className="text-9xl font-bold"
+        className="text-4xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-9xl font-extrabold italic"
         style={{ color: "var(--color-purple)" }}
       >
         {format(result?.months)}
         <span
-          className="tracking-tight"
+          className="ml-2 tracking-tight italic"
           style={{ color: "var(--color-off-black)" }}
         >
           months
         </span>
       </h1>
       <h1
-        className="text-9xl font-bold"
+        className="text-4xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-9xl font-extrabold italic"
         style={{ color: "var(--color-purple)" }}
       >
         {format(result?.days)}
         <span
-          className="tracking-tight"
+          className="ml-2 tracking-tight italic"
           style={{ color: "var(--color-off-black)" }}
         >
           days
