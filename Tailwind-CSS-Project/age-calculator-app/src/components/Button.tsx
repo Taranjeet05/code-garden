b77172a9eb3e8afd01ed32cb51dba744ec983680
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-25 17:07:09 +0100

    feat: Refactor Button component for improved styling and responsiveness

--- Tailwind-CSS-Project/age-calculator-app/src/components/Button.tsx
+++ Tailwind-CSS-Project/age-calculator-app/src/components/Button.tsx
@@ -4,18 +4,21 @@ import { FaArrowDown } from "react-icons/fa";
 
 type ButtonProps = {
   onClick: () => void;
-}
+};
 
-const Button = ({onClick}: ButtonProps) => {
+const Button = ({ onClick }: ButtonProps) => {
   return (
-    <div className="flex items-center mt-16 relative">
+    <div className="flex items-center mt-8 md:mt-12 lg:mt-16 relative px-2 sm:px-4 md:ml-6 lg:ml-10">
       <hr className="border-gray-200 border-1 w-6xl" />
       <button
-      onClick={onClick}
-        className="rounded-full p-2 flex items-center justify-center ml-4 absolute right-7"
+        onClick={onClick}
+        className="rounded-full p-3 md:p-4 flex items-center justify-center absolute 
+                 left-1/2 -translate-x-1/2 
+                 lg:left-auto lg:right-0 lg:-translate-x-0
+                 transform transition-all duration-300 hover:bg-black hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-600"
         style={{ backgroundColor: "var(--color-purple)" }}
       >
-        <FaArrowDown className="w-30 h-30 text-white font-thin " />
+        <FaArrowDown className="w-4 h-4 md:w-5 md:h-5 lg:w-6 lg:h-6 text-white font-thin" />
       </button>
     </div>
   );
