dddd747a5d41304484f95dd6f6d759c2c338a747
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-03-27 11:55:41 +0100

    refactor: Remove redundant user data fetching functions for cleaner code

--- JS-mini-projects/Just-random-code/index.js
+++ JS-mini-projects/Just-random-code/index.js
@@ -137,36 +137,4 @@ Tip: Try not to use the built-in methods like .map(), .filter(), or .reduce() in
 
 */
 
-async function getUserId() {
-  return new Promise(resolve => setTimeout(() => resolve(1), 1000));
-}
 
-async function fetchUserData(id) {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
-  const user = await response.json();
-  return user;
-}
-
-async function getUserInfo() {
-  const id = await getUserId();
-  const user = await fetchUserData(id);
-  console.log(user.name);
-}
-
-getUserInfo();
-
-
-async function fetchUser() {
-  try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
-    if (!response.ok) {
-      throw new Error("Failed to fetch user data");
-    }
-    const user = await response.json();
-    console.log(user.name);
-  } catch (error) {
-    console.error("Error:", error.message);
-  }
-}
-
-fetchUser();
