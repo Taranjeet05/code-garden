788b99e8c949ff6e9783b75b5777c9424e9f9ff2
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-04-02 21:51:15 +0200

    feat: Add ProfilePage component and routing, update Header with profile link

--- React-mini-projects/todo-app/src/Router.tsx
+++ React-mini-projects/todo-app/src/Router.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import NewTaskPage from "./pages/NewTaskPage";
 import App from "./App";
+import ProfilePage from "./pages/ProfilePage";
 
 const Router = () => {
   return (
@@ -9,6 +10,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/tasks/new" element={<NewTaskPage />} />
+        <Route path="/profile" element={<ProfilePage />} /> 
       </Routes>
     </div>
   );
--- React-mini-projects/todo-app/src/components/Header.tsx
+++ React-mini-projects/todo-app/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -6,12 +7,14 @@ const Header = () => {
       <h1 className="font-mono text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">
         ToDo
       </h1>
+      <Link to="/profile">
       <Avatar className="w-10 h-10 sm:w-12 sm:h-12 md:w-16 md:h-16 lg:w-20 lg:h-20 xl:w-24 xl:h-24">
         <AvatarImage src="https://github.com/shadcn.png" alt="User Avatar" />
         <AvatarFallback className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
           CN
         </AvatarFallback>
       </Avatar>
+      </Link>
     </div>
   );
 };
