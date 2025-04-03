90aee468e90bbfbb0e3aff21927a61f52755470f
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-04-03 19:57:51 +0200

    feat: Wrap task card in Link for navigation to TodoDetailPage

--- React-mini-projects/todo-app/src/components/TodoList.tsx
+++ React-mini-projects/todo-app/src/components/TodoList.tsx
@@ -1,5 +1,6 @@
 import { Button } from "./ui/button";
 import { CheckCircle, XCircle } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const TodoList = () => {
   return (
@@ -10,23 +11,25 @@ const TodoList = () => {
       </h1>
 
       {/* Task Card */}
-      <div className="flex justify-between items-center bg-gray-100 p-4 rounded-lg shadow-sm mb-3">
-        {/* Task Title */}
-        <div>
-          <h2 className="text-lg font-medium text-gray-900">
-            Create an app in React
-          </h2>
+      <Link to="/todo-detail">
+        <div className="flex justify-between items-center bg-gray-100 p-4 rounded-lg shadow-sm mb-3">
+          {/* Task Title */}
+          <div>
+            <h2 className="text-lg font-medium text-gray-900">
+              Create an app in React
+            </h2>
+          </div>
+          {/* Action Buttons */}
+          <div className="flex gap-2">
+            <Button className="bg-green-500 text-white hover:bg-green-600 px-4 py-2 rounded-md shadow cursor-pointer">
+              <CheckCircle className="w-8 h-8" />
+            </Button>
+            <Button className="bg-red-500 text-white hover:bg-red-600 px-4 py-2 rounded-md shadow cursor-pointer">
+              <XCircle className="w-8 h-8" />
+            </Button>
+          </div>
         </div>
-        {/* Action Buttons */}
-        <div className="flex gap-2">
-          <Button className="bg-green-500 text-white hover:bg-green-600 px-4 py-2 rounded-md shadow">
-            <CheckCircle className="w-8 h-8" />
-          </Button>
-          <Button className="bg-red-500 text-white hover:bg-red-600 px-4 py-2 rounded-md shadow">
-            <XCircle className="w-8 h-8" />
-          </Button>
-        </div>
-      </div>
+      </Link>
     </div>
   );
 };
