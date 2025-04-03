183f79ae0e89a81ac4b6736e1e5ec6c6bc45a6c6
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-04-03 19:57:14 +0200

    feat: Add TodoDetailPage route to Router for task detail viewing

--- React-mini-projects/todo-app/src/Router.tsx
+++ React-mini-projects/todo-app/src/Router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import CreateNewTaskPage from "./pages/CreateNewTaskPage";
 import App from "./App";
 import ProfilePage from "./pages/ProfilePage";
+import TodoDetailPage from "./pages/TodoDetailPage";
 
 const Router = () => {
   return (
@@ -10,7 +11,8 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/tasks/new" element={<CreateNewTaskPage />} />
-        <Route path="/profile" element={<ProfilePage />} /> 
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/todo-detail" element={<TodoDetailPage />} />
       </Routes>
     </div>
   );
