81c4cec655f3e6a613291450c2c6453baf57dd2e
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-04-04 19:52:24 +0200

    feat: Implement updateTodo controller and update routes for todo management

--- React-mini-projects/todo-app/BACKEND/src/controllers/todo.ts
+++ React-mini-projects/todo-app/BACKEND/src/controllers/todo.ts
@@ -26,11 +26,42 @@ export const createTodo: RequestHandler = (req, res) => {
   res.status(201).json({ message: "Todo Created Successfully", newTodo });
 };
 
-export const getAllTodos : RequestHandler = (req, res) => {
-  
-  if(TODO.length === 0){
-    res.status(200).json({message: "No todos found"});
+export const getAllTodos: RequestHandler = (req, res) => {
+  if (TODO.length === 0) {
+    res.status(200).json({ message: "No todos found" });
     return;
   }
-  res.status(200).json({message: "todoa Fetched Successfully", TODO});
-}
+  res.status(200).json({ message: "todoa Fetched Successfully", TODO });
+};
+
+export const updateTodo: RequestHandler<{ todoId: string }> = (req, res) => {
+  const { todoId } = req.params;
+  const { title, description } = req.body as {
+    title?: string;
+    description?: string;
+  };
+
+  const todoIndex = TODO.findIndex((todo) => todo.id === todoId);
+
+  if (todoIndex === -1) {
+    res.status(404).json({ message: "Todo not found" });
+  }
+
+  const existingTodo = TODO[todoIndex];
+
+  const updatedTodo = new Todo(
+    existingTodo.id,
+    title || existingTodo.title,
+    description || existingTodo.description,
+    existingTodo.completed,
+    existingTodo.createdAt,
+    new Date()
+  );
+
+  TODO[todoIndex] = updatedTodo;
+
+  res.status(200).json({
+    message: "✅ Todo updated successfully!",
+    updatedTodo,
+  });
+};
--- React-mini-projects/todo-app/BACKEND/src/routes/todo.ts
+++ React-mini-projects/todo-app/BACKEND/src/routes/todo.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import { createTodo, getAllTodos } from "../controllers/todo";
+import { createTodo, getAllTodos, updateTodo } from "../controllers/todo";
 
 const router = express.Router();
 
 router.route("/").post(createTodo); // Create a new todo
 router.route("/").get(getAllTodos); // Get all todos
-//router.route("/:todoId").patch(); // Update a todo
+router.route("/:todoId").patch(updateTodo); // Update a todo
 //router.route("/:todoId").delete(); // Delete a todo
 
 
