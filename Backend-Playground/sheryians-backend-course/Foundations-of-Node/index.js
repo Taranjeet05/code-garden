0a155df2ae604c30720de1409bfac33850cc5131
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-04-06 16:50:18 +0200

    feat: Implement file writing functionality and create test.txt

--- Backend-Playground/sheryians-backend-course/Foundations-of-Node/index.js
+++ Backend-Playground/sheryians-backend-course/Foundations-of-Node/index.js
@@ -1,1 +1,12 @@
-console.log("hello World");
+// writeFile, appendFile, copyFile, rename, unlink
+
+const { error, log } = require('console');
+const fs = require('fs');
+
+fs.writeFile("test.txt", "Hello I am testing out the writeFile in fs", (error) => {
+if(error) {
+  console.log("Error occured while writing the file. ❌");
+} else {
+  console.log("file is created successfully, Have a look at the file. ✅")
+}
+});
