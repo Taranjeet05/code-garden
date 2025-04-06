49754ca90e02308507d434dedfa965a9783f904b
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-04-06 16:52:15 +0200

    style: Format code for better readability in index.js

--- Backend-Playground/sheryians-backend-course/Foundations-of-Node/index.js
+++ Backend-Playground/sheryians-backend-course/Foundations-of-Node/index.js
@@ -1,20 +1,28 @@
 // writeFile, appendFile, copyFile, rename, unlink
 
-const { error, log } = require('console');
-const fs = require('fs');
+const { error, log } = require("console");
+const fs = require("fs");
 
-fs.writeFile("test.txt", "Hello I am testing out the writeFile in fs", (error) => {
-if(error) {
-  console.log("Error occured while writing the file. ❌");
-} else {
-  console.log("file is created successfully, Have a look at the file. ✅")
-}
-});
-
-fs.appendFile("test.txt", "/n I am appending this text to the file", (error) => {
-  if(error) {
-    console.log("Error occured while appending the file. ❌");
-  } else {
-    console.log(`File is appended successfully, Have a look at the file ✅`)
+fs.writeFile(
+  "test.txt",
+  "Hello I am testing out the writeFile in fs",
+  (error) => {
+    if (error) {
+      console.log("Error occured while writing the file. ❌");
+    } else {
+      console.log("file is created successfully, Have a look at the file. ✅");
+    }
   }
-});
+);
+
+fs.appendFile(
+  "test.txt",
+  "/n I am appending this text to the file",
+  (error) => {
+    if (error) {
+      console.log("Error occured while appending the file. ❌");
+    } else {
+      console.log(`File is appended successfully, Have a look at the file ✅`);
+    }
+  }
+);
