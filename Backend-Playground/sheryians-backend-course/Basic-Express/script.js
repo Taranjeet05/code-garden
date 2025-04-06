f24d5d4b63ddc09b65cf8f9b46f71ec7de722e17
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-04-06 20:46:29 +0200

    created a express server

--- Backend-Playground/sheryians-backend-course/Basic-Express/script.js
+++ Backend-Playground/sheryians-backend-course/Basic-Express/script.js
@@ -1,1 +1,18 @@
-console.log("hello World, I am testing out the server.");
+const express = require("express");
+const app = express();
+const Port = 1008;
+
+app.get("/", (req, res) => {
+  res.send(`
+    Hello world, I am creating My first Express Server.<br>
+    ---------------------------------------------------<br>
+    You are currently on the Home page.<br>
+    ---------------------------------------------------
+  `);
+});
+
+app.listen(Port, () => {
+  console.log("------------------------------------------------------------");
+  console.log(`Server is running on http://localhost:${Port}`);
+  console.log("------------------------------------------------------------");
+});
