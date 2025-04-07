9f388f1e7f57733da125812e627b0a6734db297f
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-04-07 18:12:50 +0200

    Handling form data, Worked on the UI

---
+++ Backend-Playground/sheryians-backend-course/Form-Handling-2.0/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = 1008;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(PORT, () => {
+  console.log("--------------------------------------------");
+  console.log(`server is running on the ${PORT} port.`);
+  console.log("--------------------------------------------");
+});
