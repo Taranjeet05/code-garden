7fd7404e94c3d64785ca8bd367a9e83e63288035
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-04-10 20:37:36 +0200

    feat: Implement user retrieval and creation in app.js with async handling

--- Backend-Playground/sheryians-backend-course/MongoDB/app.js
+++ Backend-Playground/sheryians-backend-course/MongoDB/app.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = 1008;
+const userModel = require("./models/user");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -13,8 +15,20 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.get("/read", (req, res) => {
-  res.render("read");
+app.get("/read", async (req, res) => {
+  
+  const readUser = await userModel.find();
+  res.render("read", {users : readUser});
+});
+
+app.post("/create", async (req, res) => {
+  const { name, email, imageUrl } = req.body;
+  let createUser = await userModel.create({
+    name,
+    email,
+    imageUrl
+  });
+  res.redirect('/read');
 });
 
 app.listen(PORT, () => {
