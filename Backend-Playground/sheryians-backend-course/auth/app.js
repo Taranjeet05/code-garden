e9142220b33f20826c0b35c4b9d47693e394fa24
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-04-11 19:35:35 +0200

    feat: Set up EJS view engine and create index view with Tailwind CSS

--- Backend-Playground/sheryians-backend-course/auth/app.js
+++ Backend-Playground/sheryians-backend-course/auth/app.js
@@ -1,50 +1,26 @@
 const express = require("express");
 const app = express();
 const PORT = 1008;
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  bcrypt.compare(
-    "Taranjeet Singh",
-    "$2b$10$itgRilhGBNHZ6mosJh0h4e8lMv4IhYZ7.GfMcV7cXZEX9wYJZ5aEK",
-    (err, result) => {
-      console.log(result);
-    }
-  );
-  res.send("Hello I am just byrpt the password");
-});
-
-app.get("/profile", (req, res) => {
-  bcrypt.genSalt(10, (error, salt) => {
-    bcrypt.hash("INDIA", salt, (error, hash) => {
-      console.log(hash);
-    });
-  });
-  res.send("hello from Profile page");
-});
-
-app.get("/jwt", (req, res) => {
-  let token = jwt.sign({ email: "taranjeet@gmail.com" }, "shhhshhh");
-  console.log(token);
-  res.cookie("token", token);
-  res.send("Wroking on JWT token.");
-});
-
-app.get('/bcrypt', async (req, res) => {
-  const salt = await bcrypt.genSalt(10);
-  const hash = await bcrypt.hash('Singh', salt);
-  console.log(hash);
-
-  res.send('Hash Created');
+  res.render("index");
 });
 
 app.listen(PORT, () => {
   console.log(
-    "---------------------------------------------------------------"
+    "-------------------------------------------------------------------------------------------------------------------------------------------------------"
   );
-  console.log(`Server is running at PORT: ${PORT}`);
+  console.log(`Server is running >> ${PORT}`);
   console.log(
-    "---------------------------------------------------------------"
+    "-------------------------------------------------------------------------------------------------------------------------------------------------------"
   );
 });
