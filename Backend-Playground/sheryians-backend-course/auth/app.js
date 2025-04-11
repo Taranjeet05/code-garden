6c2a56bdb99b4108b94056556bd2a16c21e7059a
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-04-11 22:36:55 +0200

    feat: Implement login functionality with user authentication and error handling

--- Backend-Playground/sheryians-backend-course/auth/app.js
+++ Backend-Playground/sheryians-backend-course/auth/app.js
@@ -7,6 +7,7 @@ const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
 const userModel = require("./model/user");
 const jwt = require("jsonwebtoken");
+const user = require("./model/user");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -20,6 +21,10 @@ app.get("/", (req, res) => {
 
 app.get("/profile", (req, res) => {
   res.send("Profile PAGE");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
 app.post("/create", async (req, res) => {
@@ -46,9 +51,29 @@ app.post("/create", async (req, res) => {
   }
 });
 
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+
+    if (!user) return res.send("User not found");
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) return res.send("Invalid credentials");
+
+    let token = jwt.sign({ email }, "sssssssssshhhhhhhhhhhhhhhhhhhhh");
+    res.cookie("token", token);
+    return res.redirect("/profile");
+  } catch (error) {
+    console.log("❌ Error:", error.message);
+    res.send("Internal Server Error");
+  }
+});
+
 app.get("/logout", (req, res) => {
   res.cookie("token", "");
-  res.redirect('/');
+  res.redirect("/login");
 });
 
 app.listen(PORT, () => {
