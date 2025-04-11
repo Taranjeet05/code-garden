f317c9f26ecb30b6508673d3ad4530413c0a0de6
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-04-11 21:18:47 +0200

    feat: Implement user creation endpoint with password hashing and JWT token generation

--- Backend-Playground/sheryians-backend-course/auth/app.js
+++ Backend-Playground/sheryians-backend-course/auth/app.js
@@ -4,7 +4,9 @@ const app = express();
 const PORT = 1008;
 const path = require("path");
 const cookieParser = require("cookie-parser");
-const userSchema = require('./model/user');
+const bcrypt = require("bcrypt");
+const userModel = require("./model/user");
+const jwt = require("jsonwebtoken");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -16,6 +18,39 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/profile", (req, res) => {
+  res.send("Profile PAGE");
+});
+
+app.post("/create", async (req, res) => {
+  const { name, age, email, password } = req.body;
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    let createUser = await userModel.create({
+      name,
+      age,
+      email,
+      password: hash,
+    });
+
+    let token = jwt.sign({ email }, "sssssssssshhhhhhhhhhhhhhhhhhhhh");
+
+    res.cookie("token", token);
+
+    res.redirect("/profile");
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+app.get("/logout", (req, res) => {
+  res.cookie("token", "");
+  res.redirect('/');
+});
+
 app.listen(PORT, () => {
   console.log(
     "-------------------------------------------------------------------------------------------------------------------------------------------------------"
@@ -25,4 +60,3 @@ app.listen(PORT, () => {
     "-------------------------------------------------------------------------------------------------------------------------------------------------------"
   );
 });
- 
