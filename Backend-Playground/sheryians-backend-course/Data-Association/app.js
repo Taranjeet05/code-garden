1e49bcfba3f4c67c9c3f9941a2e4a47871cb4d48
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-04-12 05:17:07 +0200

    Add user creation and post creation endpoints and testing DB

--- Backend-Playground/sheryians-backend-course/Data-Association/app.js
+++ Backend-Playground/sheryians-backend-course/Data-Association/app.js
@@ -4,9 +4,34 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const userModel = require("./models/user");
 const postModel = require("./models/post");
+const post = require("./models/post");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello from HOME PAGE");
+});
+
+app.get("/create", async (req, res) => {
+  let createUser = await userModel.create({
+    userName: "Taranjeet Singh",
+    email: "Taranjeet@t.com",
+    age: 20,
+  });
+  res.send(createUser);
+});
+
+app.get("/post/create", async (req, res) => {
+  let createPost = await postModel.create({
+    postData: "Hello kaaise hoo aap sab",
+    user: "67f9da699bd163280f19b241",
+  });
+  let user = await userModel.findOne({ _id: "67f9da699bd163280f19b241" });
+  user.posts.push(createPost._id);
+  await user.save();
+
+  res.send({ createPost, user });
 });
 
 app.listen(PORT, () => {
