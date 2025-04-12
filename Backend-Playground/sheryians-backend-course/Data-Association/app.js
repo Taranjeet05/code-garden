a614f38bf13696d34e96c8774cf90f4acface7e8
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-04-12 06:34:11 +0200

    feat: Update app routes to render views and remove unused endpoints

--- Backend-Playground/sheryians-backend-course/Data-Association/app.js
+++ Backend-Playground/sheryians-backend-course/Data-Association/app.js
@@ -4,34 +4,20 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const userModel = require("./models/user");
 const postModel = require("./models/post");
+const path = require("path");
 const post = require("./models/post");
 
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.send("Hello from HOME PAGE");
+  res.render("index");
 });
 
-app.get("/create", async (req, res) => {
-  let createUser = await userModel.create({
-    userName: "Taranjeet Singh",
-    email: "Taranjeet@t.com",
-    age: 20,
-  });
-  res.send(createUser);
-});
-
-app.get("/post/create", async (req, res) => {
-  let createPost = await postModel.create({
-    postData: "Hello kaaise hoo aap sab",
-    user: "67f9da699bd163280f19b241",
-  });
-  let user = await userModel.findOne({ _id: "67f9da699bd163280f19b241" });
-  user.posts.push(createPost._id);
-  await user.save();
-
-  res.send({ createPost, user });
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
 app.listen(PORT, () => {
