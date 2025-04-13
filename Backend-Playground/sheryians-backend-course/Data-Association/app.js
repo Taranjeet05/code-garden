cf15b2537295ee3078993650b24a4d321684b15a
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-04-13 15:16:22 +0200

    feat: Implement login functionality with JWT authentication

--- Backend-Playground/sheryians-backend-course/Data-Association/app.js
+++ Backend-Playground/sheryians-backend-course/Data-Association/app.js
@@ -56,6 +56,24 @@ app.post("/register", async (req, res) => {
   }
 });
 
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+    if (!user) return res.status(401).send("Invalid credentials ❌");
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(401).send("Invalid credentials ❌");
+
+    const token = jwt.sign({ email, userId: user._id }, process.env.JWT_SECRET);
+    res.cookie("token", token);
+
+    res.redirect("/post");
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Application is Rinning on ${PORT}`);
 });
