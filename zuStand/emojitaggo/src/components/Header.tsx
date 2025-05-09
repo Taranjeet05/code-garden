af4ec8728588525540cf0371583e8ba7f4e34f1d
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-05-09 18:17:17 +0200

    feat: Add Header component and update Home page layout

---
+++ zuStand/emojitaggo/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="p-7 text-center ">
+      <h1 className="text-indigo-800 text-7xl">Emoji Tag Board</h1>
+      <p className=" text-indigo-700 text-xl mt-4">
+        Create fun tags with emojis! 🌈✨
+      </p>
+    </div>
+  );
+};
+
+export default Header;
