898ba7aa47503524ec3b0e228d16a1ac126180ea
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-05-09 20:34:33 +0200

    feat: Update layout to set background color for improved aesthetics

--- zuStand/emojitaggo/src/app/layout.tsx
+++ zuStand/emojitaggo/src/app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        style={{
+          backgroundColor: "#eff2ff",
+        }}
       >
         {children}
       </body>
