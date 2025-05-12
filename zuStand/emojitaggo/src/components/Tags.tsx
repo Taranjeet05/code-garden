a2fd228faf6d34398fdf57dbc0868e266f64e90f
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-05-12 23:14:47 +0200

    feat: Enhance Tags component styling for better visibility of tag messages

--- zuStand/emojitaggo/src/components/Tags.tsx
+++ zuStand/emojitaggo/src/components/Tags.tsx
@@ -7,20 +7,20 @@ const Tags = () => {
         <div>
             <div>
                 <h3>Animals</h3>
-                <div>
-                    <p>No Animals tags yet</p>
+                <div className='bg-gray-500 h-20 w-50'>
+                    <p className='text-white'>No Animals tags yet</p>
                 </div>
             </div>
             <div>
                 <h3>Fruits</h3>
-                <div>
-                    <p>No Fruits tags yet</p>
+                <div className='bg-gray-500 h-20 w-50'>
+                    <p className='text-white'>No Fruits tags yet</p>
                 </div>
             </div>
             <div>
                 <h3>Countries</h3>
-                <div>
-                    <p>No Countries</p>
+                <div className='bg-gray-500 h-20 w-50'>
+                    <p className='text-name'>No Countries</p>
                 </div>
             </div>
         </div>
