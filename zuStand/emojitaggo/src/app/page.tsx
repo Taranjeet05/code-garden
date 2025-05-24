5e987bb3a00a746ee52b0beec9b940d5777cb1a3
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-05-24 23:22:22 +0200

    refactor: remove unused components and simplify Home page structure

--- zuStand/emojitaggo/src/app/page.tsx
+++ zuStand/emojitaggo/src/app/page.tsx
@@ -1,15 +1,8 @@
-import Form from "@/components/Form";
-import Header from "@/components/Header";
-import Tags from "@/components/Tags";
 
 export default function Home() {
   return (
     <>
-      <Header />
-      <div className="flex items-center justify-center h-screen bg-gray-100">
-        <Form />
-        <Tags />
-      </div>
+      <h1>...</h1>
     </>
   );
 }
