e426a1c0e9f4d7db42a8ac83c4efa7275aad039e
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-05-30 14:40:30 +0200

    feat: Add Counter component to App for enhanced functionality

--- zuStand/zustand-playground/src/App.tsx
+++ zuStand/zustand-playground/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Counter from "./components/Counter";
 import Header from "./components/Header"
 
 function App() {
   return (
     <div className="bg-black text-white h-screen">
       <Header />
+      <Counter />
     </div>
   );
 }
