f04de8721ee252bf5242c0d4ed8235cc4a944ff9
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-06-13 20:41:00 +0200

    feat: Add dislikes function to complement likes function with similar output format

--- JS-mini-projects/Just-random-code/index.js
+++ JS-mini-projects/Just-random-code/index.js
@@ -15,12 +15,42 @@ function likes(names) {
   }
 }
 
-likes([]);
+console.log(likes([]));
+console.log(likes(["Peter"]));
+console.log(likes(["Jacob", "Alex"]));
+console.log(likes(["Max", "John", "Mark"]));
+console.log(likes(["Alex", "Jacob", "Mark", "Max"]));
 
-likes(["Peter"]);
+/*
+Try this similar challenge:
 
-likes(["Jacob", "Alex"]);
+Write a function called `dislikes` that takes an array of names and returns a string describing who dislikes this, following the same pattern as the `likes` function above.
 
-likes(["Max", "John", "Mark"]);
+Example outputs:
+dislikes([]) => "no one dislikes this"
+dislikes(["Sam"]) => "Sam dislikes this"
+dislikes(["Sam", "Alex"]) => "Sam and Alex dislike this"
+dislikes(["Sam", "Alex", "Jamie"]) => "Sam, Alex and Jamie dislike this"
+dislikes(["Sam", "Alex", "Jamie", "Taylor"]) => "Sam, Alex and 2 others dislike this"
+*/
 
-likes(["Alex", "Jacob", "Mark", "Max"]);
+const dislikes = (names) => {
+  const count = names.length;
+  switch (count) {
+    case 0:
+      return "No one Dislike this";
+    case 1:
+      return `${names[0]} Dislikes this`;
+    case 2:
+      return `${names[0]} and ${names[1]} Dislike this`;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} Dislike this`;
+    default:
+      return `${names[0]}, ${names[1]} and ${count - 2} others Dislike this `;
+  }
+};
+console.log(dislikes([]));
+console.log(dislikes(["Sam"]));
+console.log(dislikes(["Sam", "Alex"]));
+console.log(dislikes(["Sam", "Alex", "Jamie"]));
+console.log(dislikes(["Sam", "Alex", "Jamie", "Taylor"]));
