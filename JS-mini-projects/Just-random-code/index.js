69a59330f566a6f128358d4fafbc12784def6c7a
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-06-15 15:49:39 +0200

    refactor: Clean up spacing and formatting in date manipulation examples

--- JS-mini-projects/Just-random-code/index.js
+++ JS-mini-projects/Just-random-code/index.js
@@ -27,17 +27,31 @@ console.log("Year:", today.getFullYear()); // to get a full year
 console.log("Month ( 0 - 11 ):", today.getMonth()); // to get a full Month(0 - 11)
 console.log("Date :", today.getDate()); // to get a date
 console.log("Day of the week: (0 - 6)", today.getDay()); // to get day of the week from like 0 = Sunday, 1 = Monday etc..
-console.log("Hours :", today.getHours()); // to get Hours 
-console.log("Minutes :", today.getMinutes()); // to get Minutes 
+console.log("Hours :", today.getHours()); // to get Hours
+console.log("Minutes :", today.getMinutes()); // to get Minutes
 console.log("seconds :", today.getSeconds()); // to get Seconds
 console.log("milliseconds :", today.getMilliseconds()); // to get milliseconds.
 
-//                                                     Setting parts of a Date 
+//                                                     Setting parts of a Date
 
-const date = new Date ();
+const date = new Date();
 
 date.setFullYear(2020); // year: 2020
 date.setMonth(10); // Month: November
 date.setDate(10); // date : 10th
 
 console.log(date);
+
+//                                                    Date Comparison
+
+const date1 = new Date("2005-01-15");
+const date2 = new Date("2005-11-10");
+
+if (date1 > date2) {
+  console.log("date1 is bigger than date2");
+  return;
+} else {
+  console.log("Date 2  is bigger than date1");
+}
+
+console.log(date1.getTime() === date2.getTime()); // false
