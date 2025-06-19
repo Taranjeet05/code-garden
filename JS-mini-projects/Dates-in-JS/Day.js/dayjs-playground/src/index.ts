9c82eb6424983d4c235a68dbaca717f6a4d1c391
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-06-19 18:00:36 +0200

    Enhance dayjs playground: add detailed date manipulation examples and extend functionality with toArray plugin

--- JS-mini-projects/Dates-in-JS/Day.js/dayjs-playground/src/index.ts
+++ JS-mini-projects/Dates-in-JS/Day.js/dayjs-playground/src/index.ts
@@ -1,5 +1,31 @@
 import dayjs, { Dayjs } from "dayjs";
+import toArray from "dayjs/plugin/toArray.js";
 
+dayjs.extend(toArray);
 
-const today : Dayjs = dayjs();
-console.log(today)
+// Demonstrates basic usage of the Dayjs library for date and time manipulation.
+const today: Dayjs = dayjs();
+console.log(today.format("dddd, MMMM-YYYY-DD"));
+console.log(
+  today.year(),
+  today.month(),
+  today.date(),
+  today.hour(),
+  today.minute(),
+  today.second(),
+  today.millisecond()
+);
+
+// middlewares or dayjs plugins for extra functionality in dayjs ->
+//! but each plugin increases the library's footprint.
+
+console.log(today.toArray()); // we will get year,month,date,hour,minute,second,millisecond in an array format...
+console.log(today.daysInMonth());
+
+// Manipulate
+
+console.log(today.startOf("year").format("dddd, MMMM-YYYY-DD"));
+console.log(today.add(10, "day").format("dddd, MMMM-YYYY-DD"));
+console.log(today.subtract(10, "day").format("dddd, MMMM-YYYY-DD"));
+console.log(today.endOf('year').format("dddd, MMMM-YYYY-DD"));
+
