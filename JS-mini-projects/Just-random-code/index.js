b7eb7ac2d857e42c0962f34a5881d69af2cef8bf
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-06-26 18:24:47 +0200

    feat: add documentation for Map in JavaScript

---
+++ JS-mini-projects/Just-random-code/index.js
@@ -0,0 +1,10 @@
+//!                           What is a Map in JavaScript?
+// >>> A Map is a built-in JavaScript object that allows you to store key-value pairs — just like an object, but:
+
+// Key differences from a regular object ({}):
+//! Feature	                                         Object ({})	                                                       Map (new Map())
+// Keys can be	                                      Only strings or symbols	                                            Any data type (even objects, arrays)
+// Order of keys	                                  Not guaranteed	                                                    Insertion order is preserved
+// Size	                                              Object.keys(obj).length	                                            .size property
+// Iteration	                                      Harder	                                                            Easier (e.g., map.forEach, map.entries())
+
