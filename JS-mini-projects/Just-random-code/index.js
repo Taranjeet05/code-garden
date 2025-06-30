76deb264e346dc2b10f1b0f7268c3802fdf2fbff
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-06-30 22:04:21 +0200

    feat: add user mapping example to demonstrate extracting names from an array of user objects

--- JS-mini-projects/Just-random-code/index.js
+++ JS-mini-projects/Just-random-code/index.js
@@ -6,6 +6,15 @@ const doubled = numbers.map((number) => {
   return number * 2;
 });
 
-console.log(doubled); 
+console.log(doubled);
 
+const users = [
+  { name: "joe", age: 20 },
+  { name: "jane", age: 25 },
+  { name: "jack", age: 30 },
+  { name: "jill", age: 35 },
+];
 
+const userNames = users.map((user) => user.name);
+
+console.log(userNames);
