59713509dac0956ad3bf5f467e50530c90959bbb
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-07-06 17:49:17 +0200

    for loop simple questions

---
+++ JS-mini-projects/Just-random-code/index.js
@@ -0,0 +1,12 @@
+// print 1 to ten in for loop
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+// print even numbers from 1 to 20
+
+for (let i = 2; i <= 20; i += 2) {
+  console.log(i);
+}
+
