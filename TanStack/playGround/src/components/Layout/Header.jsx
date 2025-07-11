418b0b7331990a3ab60c6c8ec714991237d68384
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-07-11 10:56:23 +0200

    feat: implement Header component with navigation links

--- TanStack/playGround/src/components/Layout/Header.jsx
+++ TanStack/playGround/src/components/Layout/Header.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div className="flex items-center justify-between text-6xl p-5 text-white">
+        <NavLink to="/">TanStack</NavLink>
+        <nav>
+          <ul className="flex space-x-4 text-3xl text-white">
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-green-400 font-bold underline"
+                    : "text-gray-300 hover:text-green-200"
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/trad"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-green-400 font-bold underline"
+                    : "text-gray-300 hover:text-green-200"
+                }
+              >
+                FetchOld
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/rq"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-green-400 font-bold underline"
+                    : "text-gray-300 hover:text-green-200"
+                }
+              >
+                FetchRq
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
