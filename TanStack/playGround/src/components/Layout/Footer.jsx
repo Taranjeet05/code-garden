f4db798ab72e346931a97c1a2888b2363772d61d
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-07-11 11:02:50 +0200

    feat: update Footer component to display navigation text

--- TanStack/playGround/src/components/Layout/Footer.jsx
+++ TanStack/playGround/src/components/Layout/Footer.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="flex justify-center items-center">
+      <h3 className="text-green-400 hover:text-green-300">
+        PlayGround @LearningTanStack
+      </h3>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
