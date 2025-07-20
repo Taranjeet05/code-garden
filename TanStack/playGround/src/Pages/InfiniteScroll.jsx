633e51eb4f24f88b4ccac9e3d75de6246fe84ee4
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-07-20 10:15:44 +0200

    Created a new page for infinite Scrolling

---
+++ TanStack/playGround/src/Pages/InfiniteScroll.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InfiniteScroll = () => {
+  return (
+    <div>
+        <p className='text-white text-2xl font-mono p-10'>Hello, from Infinite Scroll.</p>
+    </div>
+  )
+}
+
+export default InfiniteScroll
