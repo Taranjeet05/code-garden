337506fd7d1890c0eb566e0ab587a509a195255e
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-07-22 10:01:07 +0200

    Refactor Header component to include sidebar toggle functionality and improve layout structure

--- React-mini-projects/Hooks/src/components/layout/Header.jsx
+++ React-mini-projects/Hooks/src/components/layout/Header.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import SideBar from "./ui/SideBar";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div>
-      <h1 className="font-bold text-4xl flex items-center justify-center p-10 bg-emerald-100">Hook's World</h1>
+      <div className="flex items-center justify-between bg-emerald-100 p-10">
+        <h1 className="font-bold text-4xl   ">
+          <NavLink to="/" className="text-emerald-800 hover:text-emerald-600">
+            Hook's world
+          </NavLink>
+        </h1>
+        <div>
+          <button
+            onClick={() => setOpen(!open)}
+            className="text-2xl text-emerald-800 hover:text-emerald-600 font-serif "
+          >
+            {open ? (
+              "close ✖️"
+            ) : (
+              <>
+                Navigate Pages <span className="">🔍</span>
+              </>
+            )}
+          </button>
+        </div>
+      </div>
+      <div>{open && <SideBar />}</div>
     </div>
   );
 };
