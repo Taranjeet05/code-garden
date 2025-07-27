d8733fbddee89822bf94158ee05833ee1e2e1927
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-07-27 14:38:18 +0200

    Remove toCamelCase function and its usage from index.js

--- JS-mini-projects/Just-random-code/index.js
+++ JS-mini-projects/Just-random-code/index.js
@@ -1,8 +1,1 @@
-function toCamelCase(str) {
-    return str.split(/[-_]/).map((word, index) => {
-        if(index === 0) return word;
-        return word[0].toUpperCase() + word.slice(1);
-    }).join('');
-}
-
-console.log(toCamelCase('hello-world'));
+ 
