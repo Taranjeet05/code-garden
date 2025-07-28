d515e9f3a806b1a7646e770837c01684077f9178
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-07-28 13:08:57 +0200

    Refactor UseEffect component to manage name and count states with corresponding effects

--- React-mini-projects/Hooks/src/pages/UseEffect.jsx
+++ React-mini-projects/Hooks/src/pages/UseEffect.jsx
@@ -1,10 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const UseEffect = () => {
+  const [name, setName] = useState("");
+  const [count, setCount] = useState(0);
+
+  function incrementCount() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  // This effect will run when the component mounts
   useEffect(() => {
-    // This will run once when the component mounts
     console.log("Page loaded 🟩");
   }, []);
+
+  // This effect will run when the component when the name changes to a new value and not when it is the same value
+  // This is because of the dependency array [name]
+  useEffect(() => {
+    console.log("Name Changed 🟦", name);
+  }, [name]);
+
+  // This effect will run when the count changes, This is because of the dependency array [count]
+  useEffect(() => {
+    console.log("Count Changed 🔴", count);
+  }, [count]);
 
   return (
     <div className="p-10 mt-5">
@@ -14,9 +32,14 @@ const UseEffect = () => {
         type="text"
         placeholder="Type you name over Here"
         className="p-2 border border-gray-400 rounded mb-4 w-1/3"
+        onChange={(e) => setName(e.target.value)}
       />
       <div className="mb-4">
-        <button className="bg-green-500 text-white px-4 py-2 rounded font-bold">
+        <p className="my-14 font-bold text-5xl ml-16">{count}</p>
+        <button
+          className="bg-green-500 text-white px-4 py-2 rounded font-bold"
+          onClick={incrementCount}
+        >
           Increment Count
         </button>
       </div>
