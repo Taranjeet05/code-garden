1f519fa4451cef6ae91644cecb8770891eab5860
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-09-03 18:52:41 +0200

    Add oppositeNumber function to return the opposite of a given number with tests

---
+++ JS-mini-projects/Just-random-code/index.js
@@ -0,0 +1,13 @@
+// Problem:
+// Write a function opposite(number) that returns the opposite (negative if positive, positive if negative) of the given number.
+
+const oppositeNumber = (number) => {
+  return -number;
+};
+
+// Test
+console.log(oppositeNumber(55));
+console.log(oppositeNumber(-1));
+console.log(oppositeNumber(-2005));
+console.log(oppositeNumber(1008));
+console.log(oppositeNumber(2025));
