974e043be5eeaca4bf429eca281b238e7cb984a6
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-09-03 19:20:55 +0200

    Add positiveSum function to return the sum of all positive numbers in an array

--- JS-mini-projects/Just-random-code/index.js
+++ JS-mini-projects/Just-random-code/index.js
@@ -37,3 +37,24 @@ console.log(findMissingLetter(["a", "z", "c", "e"]));
 console.log(findMissingLetter(["a", "b", "c", "d", "k"]));
 console.log(findMissingLetter(["x", "y", "z"]));
 console.log(findMissingLetter(["1", "2", "3", "5"]));
+
+// Problem:
+// You get an array of numbers. Return the sum of all the positive ones.
+// If there are no positive numbers, return 0.
+
+const positiveSum = (arr) => {
+  let sum = 0;
+  for (let num of arr) {
+    if (num > 0) {
+      sum += num;
+    }
+  }
+  return sum;
+};
+
+// test
+
+console.log(positiveSum([1, -4, 7, 12])); // 20
+console.log(positiveSum([-1, -2, -3])); // 0
+console.log(positiveSum([10, 5, -2])); // 5
+console.log(positiveSum([]));
