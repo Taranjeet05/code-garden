5144d6daf1dd76d737b24085774ffb3f07f9c2a3
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-09-07 19:55:23 +0200

    Refactor sum function and add friend function with test cases

--- JS-mini-projects/Just-random-code/index.js
+++ JS-mini-projects/Just-random-code/index.js
@@ -1,6 +1,14 @@
 function sum(numbers) {
-    return numbers.reduce((acc, curr) => acc + curr, 0);
+  return numbers.reduce((acc, curr) => acc + curr, 0);
 }
 
 // test
-console.log(sum([1, 5.2, 4, 0, -1]))
+console.log(sum([1, 5.2, 4, 0, -1]));
+
+function friend(friends) {
+    return friends.filter(name => name.length === 4);
+}
+
+// test
+
+console.log(friend(["Love", "Your", "Face", "1"]));
