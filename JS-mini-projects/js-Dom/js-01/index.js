066e9a3eb4c7f4604517d3b9a6295465bacdd708
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-09-20 21:06:27 +0200

    DOM manipulation: get/set/remove attributes, createElement, append/remove/prepend children, style modification, and classList operations (add/remove/toggle)

--- JS-mini-projects/js-Dom/js-01/index.js
+++ JS-mini-projects/js-Dom/js-01/index.js
@@ -1,16 +1,9 @@
-const h1 = document.querySelector("h1");
-let a = document.querySelector("a");
-let img = document.querySelector("img");
+let h1 = document.createElement('h1')
+h1.innerText = 'Dom is easy to flex'
 
-img.setAttribute(
-  "src",
-  "https://images.unsplash.com/photo-1730904631875-10bee14067eb?q=80&w=428&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-);
+let div = document.createElement('div');
+div.appendChild(h1);
 
-console.log(a.getAttribute("href"));
-a.removeAttribute("href");
+document.body.append(div)
 
-console.dir(a)
-
-h1.innerHTML = "<i>hey, Bharat</>";
-console.dir(h1);
+div.classList.add('ping')
