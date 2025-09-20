f083c0a02220d79a368739d10cc58e8f5501c551
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-09-20 22:24:23 +0200

    Implement comprehensive DOM manipulation: text content modification, innerHTML usage, attribute operations (get, set, remove) for images and buttons

--- JS-mini-projects/js-Dom/js-01/index.js
+++ JS-mini-projects/js-Dom/js-01/index.js
@@ -7,9 +7,35 @@ h1.innerText = "Welcome to Sheryians coding school";
 
 // task 2 = select all <li> elements and print their text using a loop.
 
-let liList = document.querySelectorAll('li');
+let liList = document.querySelectorAll("li");
 
-for (let i = 0; i < liList.length; i++){
-    console.log(liList[i].textContent)
+for (let i = 0; i < liList.length; i++) {
+  console.log(liList[i].textContent);
 }
 
+// task 3 = select a paragraph and replace its content with:
+// {  <b> updated </b> by javascript  }
+
+const p = document.querySelector("p");
+
+p.innerHTML = "<b> updated </b> by Javascript";
+
+// how to get an src of an img
+
+const img = document.querySelector("img");
+
+console.log(img.getAttribute("src"));
+
+// what does setAttribute does ?
+
+// It will set the attribute of anything what we select for example
+
+document
+  .querySelector("img")
+  .setAttribute(
+    "src",
+    "https://plus.unsplash.com/premium_photo-1669387727429-0a444196038d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8QmhhcmF0fGVufDB8fDB8fHww"
+  );
+
+// remove the disabled attribute from a button.
+document.querySelector("button").removeAttribute("disabled");
