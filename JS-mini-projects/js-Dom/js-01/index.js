dc2cc006181e142d0e8f1f8325f7355ffa650b5a
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-09-20 23:18:42 +0200

    Implement comprehensive DOM operations: removeChild, dynamic list management, complex element creation with nested structure, and multiple append operations

--- JS-mini-projects/js-Dom/js-01/index.js
+++ JS-mini-projects/js-Dom/js-01/index.js
@@ -1,7 +1,6 @@
 // task 1 = select the heading of a page by ID and change its text to
 // 'Welcome to Sheryians'
 
-
 const h1 = document.querySelector("#id");
 
 h1.innerText = "Welcome to Sheryians coding school";
@@ -41,15 +40,38 @@ document
 // remove the disabled attribute from a button.
 document.querySelector("button").removeAttribute("disabled");
 
+// what does createElement() do ? what's returned ?
 
-// what does createElement() do ? what's returned ? 
+let h1element = document.createElement("h1");
 
-let h1element = document.createElement('h1');
+h1element.textContent = "Create Element Done";
 
-h1element.textContent = 'Create Element Done'
-
-document.body.appendChild(h1element)
+document.body.appendChild(h1element);
 
 // remove an element using removeChild() ?
 
-document.body.removeChild(document.querySelector('img'))
+document.body.removeChild(document.querySelector("img"));
+
+// create a new list item <li>New Task </li> and add it to the end of a <ul>.
+
+const li = document.createElement("li");
+
+li.textContent = "NEW TASK";
+
+document.querySelector("ul").appendChild(li);
+
+// create a new img element with a placeholder source and add it at the top of a div.
+
+const div = document.createElement("div");
+const img2 = document.createElement("img");
+const pp = document.createElement("p");
+
+img2.setAttribute(
+  "src",
+  "https://images.unsplash.com/photo-1710225358761-4f5891df657d?q=80&w=464&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+);
+
+pp.textContent = "Hello, This is a paragraph in a div. OHH yeah"
+
+div.append(img2, pp)
+document.body.append(div)
