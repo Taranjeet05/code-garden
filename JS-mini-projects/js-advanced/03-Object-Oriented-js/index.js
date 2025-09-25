c3d50b60a384f444bba27b75199a535a40825cdf
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-09-25 19:09:05 +0200

    Add constructor function for Pencil objects with prototype method
    - Implement CreatePencil constructor with name, price, color, company properties
    - Add write() method that creates colored h1 elements with text
    - Create pencil instances with different configurations
    - Add prototype property "madeIn" shared across all pencil instances
    - Demonstrate object creation and method usage with console logging

---
+++ JS-mini-projects/js-advanced/03-Object-Oriented-js/index.js
@@ -0,0 +1,34 @@
+// I would like to say it is like creating an factory with a bluePrint of how 
+/// the object would look like and with that bluePrint, so that we can create 
+///  different values, this is called OOPS basically
+
+// !-- Constructor function 
+function CreatePencil (name, price,color,company) {
+    this.name = name; // here the value of this is blank object created by NEW keyword
+    this.price = price;
+    this.color = color;
+    this.company = company;
+    this.write = function(text){
+        let h1 = document.createElement('h1');
+        h1.textContent = text
+        h1.style.color = color
+        document.body.append(h1);
+    }
+}
+
+let pencil1 = new CreatePencil('Nat', 10 ,'orange', 'Nat raj');
+let pencil2 = new CreatePencil('Doom', 10 ,'white', 'Nat raj');
+let pencil3 = new CreatePencil('slb', 10 ,'green', 'bb');
+
+console.log(pencil1);
+console.log(pencil2);
+
+pencil1.write(`My name is ${pencil1.name} and I am affordable with ${pencil1.price} rupees.`)
+pencil2.write(`Meet me ${pencil2.name}, I believe in comfort, and speed within ${pencil2.price} rupees`)
+pencil3.write(`${pencil3.name} : Fast, Comfortable, and affordable within ${pencil3.price} rupees`)
+
+// !-- protoTypes :
+// Prototype is used when we want to share methods or properties across all objects created 
+// by a constructor, so that they don’t get duplicated in every instance.
+
+CreatePencil.prototype.madeIn ='Made in India with Love'
