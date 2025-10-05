e5b2364f71b9c213165f5f4594a62049484a2c9f
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-10-05 08:11:38 +0200

    completed codewars challenge

--- JS-mini-projects/logic_practice/01-basics/index.js
+++ JS-mini-projects/logic_practice/01-basics/index.js
@@ -193,3 +193,58 @@ const getCount = (str) => {
 };
 
 getCount("Hello, this  is the ");
+
+// Implement a pseudo-encryption algorithm which given a string S and an integer N concatenates all the odd-indexed characters of S with all the even-indexed characters of S, this process should be repeated N times.
+
+// Examples:
+
+// encrypt("012345", 1)  =>  "135024"
+// encrypt("012345", 2)  =>  "135024"  ->  "304152"
+// encrypt("012345", 3)  =>  "135024"  ->  "304152"  ->  "012345"
+
+// encrypt("01234", 1)  =>  "13024"
+// encrypt("01234", 2)  =>  "13024"  ->  "32104"
+// encrypt("01234", 3)  =>  "13024"  ->  "32104"  ->  "20314"
+// Together with the encryption function, you should also implement a decryption function which reverses the process.
+
+// If the string S is an empty value or the integer N is not positive, return the first argument without changes.
+
+function encrypt(text, n) {
+  if (!text || n === 0) return text;
+
+  for (let i = 0; i < n; i++) {
+    let odd = "";
+    let even = "";
+
+    for (let j = 0; j < text.length; j++) {
+      if (j % 2 === 0) even += text[j];
+      else odd += text[j];
+    }
+    text = odd + even;
+  }
+  return text;
+}
+
+function decrypt(encryptedText, n) {
+  if (!encryptedText || n <= 0) return encryptedText;
+
+  for (let i = 0; i < n; i++) {
+    const half = Math.floor(encryptedText.length / 2);
+    const oddHalf = encryptedText.slice(0, half);
+    const evenHalf = encryptedText.slice(half);
+    let result = "";
+
+    for (let j = 0; j < encryptedText.length; j++) {
+      if (j % 2 === 0) {
+        result += evenHalf[Math.floor(j / 2)] || "";
+      } else {
+        result += oddHalf[Math.floor(j / 2)] || "";
+      }
+    }
+    encryptedText = result;
+  }
+  return encryptedText;
+}
+
+console.log(encrypt("012345", 6));
+console.log(decrypt("012345", 6));
