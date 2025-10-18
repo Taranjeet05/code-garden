d70a92d971e531d7bf4878407a74e80b3bb7a66d
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-10-18 19:38:12 +0200

    empty index.js file

--- JS-mini-projects/Just-random-code/index.js
+++
@@ -1,8 +0,0 @@
-function highAndLow(numbers) {
-  const nums = numbers.split(" ").map(Number);
-  const max = Math.max(...nums);
-  const min = Math.min(...nums);
-  return `${max} ${min}`;
-}
-
-console.log(highAndLow("1 2 3 4 5"));
