a1a61f03bfe7fce120fa27dc6641d9012e5ce800
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-11-26 19:44:03 +0100

    Added GSAP animation Learned in 001

--- GSAP/001-Project/index.js
+++ GSAP/001-Project/index.js
@@ -1,1 +1,32 @@
-alert('GSAP')
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.to(".hero-page-img", {
+    clipPath:
+      "polygon(0% 15%, 0 0, 15% 0%, 85% 0%, 100% 0, 100% 15%, 100% 85%, 100% 100%, 85% 100%, 15% 100%, 0 100%, 0% 85%)",
+  });
+
+  gsap.from(".navbar", {
+    y: -100,
+    duration: 2,
+  });
+
+  gsap.fromTo(
+    ".nav-button",
+    {
+      x: -500,
+    },
+    {
+      x: 50,
+      duration: 2,
+    }
+  );
+
+  gsap.from(".logo", {
+    y: 100,
+    duration: 2,
+  });
+
+  gsap.from('#section', {
+    y:500,
+    duration: 3
+  })
+});
