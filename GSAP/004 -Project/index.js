b9a2867e4ff6c69b55487b68d3546d78f75b3325
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-11-28 20:43:17 +0100

    Project for the Scroll Trigger with pin

---
+++ GSAP/004 -Project/index.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#page1",
+      start: "2% top",
+      end: "+=3000",
+      markers: true,
+      scrub: 2,
+      pin: true,
+    },
+  });
+
+  tl.fromTo(
+    "#pics img",
+    {
+      x: 1600,
+    },
+    {
+      x: -1600,
+    //   duration: 3,
+    }
+  );
+});
